f5ce432a7f31e6449f08d6612fe767a13c8d7483
Nick Satterly <nick.satterly@guardian.co.uk>
2012-05-14 13:26:36 +0100

    Add Syslog receiver

---
+++ bin/alert-syslog.py
@@ -0,0 +1,241 @@
+#!/usr/bin/env python
+########################################
+#
+# alert-syslog.py - Alert Syslog Receiver
+#
+########################################
+
+# TODO
+# 1. parse SD PARAMS
+# 2. add configurable parsing
+
+import os
+import sys
+try:
+    import json
+except ImportError:
+    import simplejson as json
+import stomp
+import time
+import datetime
+import logging
+import socket
+import select
+import uuid
+import re
+
+__version__ = '1.0.0'
+
+BROKER_LIST  = [('localhost', 61613)] # list of brokers for failover
+ALERT_QUEUE  = '/queue/alerts'
+EXPIRATION_TIME = 600 # seconds = 10 minutes
+
+LOGFILE = '/var/log/alerta/alert-syslog.log'
+PIDFILE = '/var/run/alerta/alert-syslog.pid'
+DISABLE = '/opt/alerta/conf/alert-syslog.disable'
+
+SEVERITY_CODE = {
+    # ITU RFC5674 -> Syslog RFC5424
+    'CRITICAL':       1, # Alert
+    'MAJOR':          2, # Crtical
+    'MINOR':          3, # Error
+    'WARNING':        4, # Warning
+    'NORMAL':         5, # Notice
+    'INFORM':         6, # Informational
+    'DEBUG':          7, # Debug
+}
+
+SYSLOG_FACILITY_NAMES = [
+    "kern",
+    "user",
+    "mail",
+    "daemon",
+    "auth",
+    "syslog",
+    "lpr",
+    "news",
+    "uucp",
+    "cron",
+    "authpriv",
+    "ftp",
+    "ntp",
+    "audit",
+    "alert",
+    "clock",
+    "local0",
+    "local1",
+    "local2",
+    "local3",
+    "local4",
+    "local5",
+    "local6",
+    "local7"
+]
+
+SYSLOG_SEVERITY_NAMES = [
+    "emerg",
+    "alert",
+    "crit",
+    "err",
+    "warning",
+    "notice",
+    "info",
+    "debug"
+]
+
+SYSLOG_SEVERITY_MAP = {
+    'emerg':   'CRITICAL',
+    'alert':   'CRITICAL',
+    'crit':    'MAJOR',
+    'err':     'MINOR',
+    'warning': 'WARNING',
+    'notice':  'NORMAL',
+    'info':    'INFORM',
+    'debug':   'DEBUG',
+}
+
+SYSLOG_UDP_PORT             = 514
+SYSLOG_TCP_PORT             = 514
+
+def send_syslog(data):
+    global conn
+
+    # May 14 07:52:53 WEBSYSUNS rsyslogd: [origin software="rsyslogd" swVersion="4.2.0" x-pid="589" x-info="http://www.rsyslog.com"] rsyslogd was HUPed, type 'lightweight'.
+
+    m = re.match(r'<(\d+)>(\S{3}) (\d+) (\d+:\d+:\d+) (\S+) (\S+): (.*)', data)
+    if m:
+        PRI = int(m.group(1))
+        LOGHOST = m.group(5)
+        TAG = m.group(6)
+        MSG = m.group(7)
+        fac = PRI >> 3
+        sev = PRI & 7
+        facility = SYSLOG_FACILITY_NAMES[fac]
+        severity = SYSLOG_SEVERITY_NAMES[sev]
+    else:
+        logging.error("Could not decode syslog message: %s", data)
+        return
+
+    environment = 'INFRA'
+    service = 'Servers'
+    resource =  LOGHOST
+    event = '%s%s' % (facility.capitalize(), severity.capitalize())
+    group = 'Syslog'
+    value = TAG
+    text = MSG
+    tags = [ '%s.%s' % (facility, severity) ]
+
+    alertid = str(uuid.uuid4()) # random UUID
+
+    headers = dict()
+    headers['type']           = "syslogAlert"
+    headers['correlation-id'] = alertid
+    headers['persistent']     = 'true'
+    headers['expires']        = int(time.time() * 1000) + EXPIRATION_TIME * 1000
+
+    alert = dict()
+    alert['id']            = alertid
+    alert['resource']      = (environment + '.' + service + '.' + resource).lower()
+    alert['event']         = event
+    alert['group']         = group
+    alert['value']         = value
+    alert['severity']      = SYSLOG_SEVERITY_MAP[severity]
+    alert['severityCode']  = SEVERITY_CODE[alert['severity']]
+    alert['environment']   = environment.upper()
+    alert['service']       = service
+    alert['text']          = text
+    alert['type']          = 'syslogAlert'
+    alert['tags']          = tags
+    alert['summary']       = '%s - %s %s is %s on %s %s' % (environment, alert['severity'].upper(), event, value, service, resource)
+    alert['createTime']    = datetime.datetime.utcnow().isoformat()+'Z'
+    alert['origin']        = 'alert-syslog/%s' % os.uname()[1]
+    alert['thresholdInfo'] = 'n/a'
+
+    logging.info('%s : %s', alertid, json.dumps(alert))
+
+    try:
+        conn = stomp.Connection(BROKER_LIST)
+        conn.start()
+        conn.connect(wait=True)
+    except Exception, e:
+        print >>sys.stderr, "ERROR: Could not connect to broker - %s" % e
+        logging.error('Could not connect to broker %s', e)
+        sys.exit(1)
+    try:
+        conn.send(json.dumps(alert), headers, destination=ALERT_QUEUE)
+    except Exception, e:
+        print >>sys.stderr, "ERROR: Failed to send alert to broker - %s " % e
+        logging.error('Failed to send alert to broker %s', e)
+        sys.exit(1)
+    broker = conn.get_host_and_port()
+    logging.info('%s : Alert sent to %s:%s', alertid, broker[0], str(broker[1]))
+
+    return
+
+def main():
+    global conn
+
+    logging.basicConfig(level=logging.DEBUG, format="%(asctime)s alert-syslog[%(process)d] %(levelname)s - %(message)s", filename=LOGFILE)
+    logging.info('Starting up Alert Syslog version %s', __version__)
+
+    # Write pid file
+    if os.path.isfile(PIDFILE):
+        logging.error('%s already exists, exiting', PIDFILE)
+        sys.exit(1)
+    else:
+        file(PIDFILE, 'w').write(str(os.getpid()))
+
+    while os.path.isfile(DISABLE):
+        logging.warning('Disable flag exists (%s). Sleeping...', DISABLE)
+        time.sleep(120)
+
+    # Set up syslog UDP listener
+    try:
+        udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        udp.bind(('', SYSLOG_UDP_PORT))
+    except socket.error, e:
+        logging.error('Syslog UDP error: %s', e)
+        sys.exit(2)
+    logging.info('Listening on syslog port 514/udp...')
+
+    # Set up syslog TCP listener
+    try:
+        tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        tcp.bind(('', SYSLOG_TCP_PORT))
+        tcp.listen(5)
+    except socket.error, e:
+        logging.error('Syslog TCP error: %s', e)
+        sys.exit(2)
+    logging.info('Listening on syslog port 514/tcp...')
+
+    # Connect to message broker
+    try:
+        conn = stomp.Connection(BROKER_LIST)
+        conn.start()
+        conn.connect(wait=True)
+    except Exception, e:
+        logging.error('Stomp connection error: %s', e)
+
+    while True:
+        try:
+            ip, op, rdy = select.select([udp,tcp], [], [])
+            for i in ip:
+                if i == udp:
+                    data = udp.recv(4096)
+                    logging.debug('Syslog UDP: %s', data)
+                    send_syslog(data)
+                if i == tcp:
+                    client,addr = tcp.accept()
+                    data = client.recv(4096)
+                    client.close()
+                    logging.debug('Syslog TCP: %s', data)
+                    send_syslog(data)
+
+            time.sleep(0.01)
+        except (KeyboardInterrupt, SystemExit):
+            conn.disconnect()
+            os.unlink(PIDFILE)
+            sys.exit(0)
+
+if __name__ == '__main__':
+    main()
