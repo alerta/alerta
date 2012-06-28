ed335c0f512a2dfd23a0fcc6dd1c053c1146ba91
Nick Satterly <nick.satterly@guardian.co.uk>
2012-06-28 17:39:54 +0100

    Enhanced query support for alert DB API and bug fixes

--- htdocs/api/v1/alert-dbapi.py
+++ htdocs/api/v1/alert-dbapi.py
@@ -13,13 +13,13 @@ except ImportError:
     import simplejson as json
 import time
 import datetime
-import copy
 import pymongo
+import urlparse
 import cgi, cgitb
 import logging
 import re
 
-__version__ = '1.2'
+__version__ = '1.3.0'
 
 LOGFILE = '/var/log/alerta/alert-dbapi.log'
 
@@ -27,210 +27,229 @@ LOGFILE = '/var/log/alerta/alert-dbapi.log'
 class DateEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, (datetime.date, datetime.datetime)):
-            return obj.isoformat()+'Z'
+            return obj.replace(microsecond=0).isoformat() + ".%03dZ" % (obj.microsecond//1000)
         else:
             return json.JSONEncoder.default(self, obj)
 
 def main():
 
-    logging.basicConfig(level=logging.DEBUG, format="%(asctime)s alert-dbapi[%(process)d] %(levelname)s - %(message)s", filename=LOGFILE)
+    start = time.time()
 
-    cgiform = cgi.FieldStorage() # only used by POST to smuggle 'delete' as "_method" variable
-    callback = None
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s alert-dbapi[%(process)d] %(levelname)s - %(message)s", filename=LOGFILE)
+    logging.info('Received HTTP request %s %s' % (os.environ['REQUEST_METHOD'], os.environ['REQUEST_URI']))
 
-    # Connection to MongoDB
+    total = 0
+    status = dict()
+    status['response'] = dict()
+    status['response']['status'] = None
+    error = 'unknown error'
+
+    for e in os.environ:
+        logging.debug('%s: %s', e, os.environ[e])
+
+    # Get HTTP method and any body data
+    method = os.environ['REQUEST_METHOD']
+    if method in ['PUT', 'POST']:
+        try:
+            data = json.loads(sys.stdin.read())
+        except ValueError, e:
+            data = list()
+            logging.warning('Failed to get data - %s', e)
+            error = 'failed to parse json data in body'
+        if '_method' in data:                  # for clients that don't support a DELETE, use POST with "_method: delete"
+            method = data['_method'].upper()
+
+    # Parse RESTful URI
+    uri = urlparse.urlsplit(os.environ['REQUEST_URI'])
+    form = urlparse.parse_qs(os.environ['QUERY_STRING'])
+    request = method + ' ' + uri.path
+
+    # Connect to MongoDB
     mongo = pymongo.Connection()
     db = mongo.monitoring
     alerts = db.alerts
     mgmt = db.status
+    query = dict()
 
-    for e in os.environ:
-        logging.info('%s: %s', e, os.environ[e])
+    m = re.search(r'GET /alerta/api/v1/alerts/alert/(?P<id>\S+)$', request)
+    if m:
+        query['_id'] = m.group('id')
 
-    total = 0
-    status = dict()
-    start = time.time()
-    status['response'] = dict()
-    status['response']['status'] = 'failed' # assume 'failed', unless a response overwrites with 'ok'
+        status['response']['alert'] = list()
 
-    if 'REQUEST_URI' in os.environ and 'REQUEST_METHOD' in os.environ:
-
-        if os.environ['REQUEST_URI'].startswith('/alerta/api/v1/alerts/alert/'):
-
-            if os.environ['REQUEST_METHOD'] == 'GET':
-
-                # REQUEST_URI: /alerta/api/v1/alerts/alert/3bfd0f9d-2843-419d-9d4d-c99079a39d36
-                # QUERY_STRING: id=3bfd0f9d-2843-419d-9d4d-c99079a39d36 (after RewriteRule applied)
-
-                queryStr = os.environ['QUERY_STRING']
-                form = dict([queryParam.split('=') for queryParam in queryStr.split('&')])
-                form['_id'] = form['id']
-                del form['id'] 
-                logging.info('form %s', json.dumps(form))
-
-                status['response']['alert'] = list()
-
-                logging.debug('MongoDB GET -> alerts.find_one(%s)', form)
-                alert = alerts.find_one(form)
-                if alert:
-                    alert['id'] = alert['_id']
-                    del alert['_id']
-                    status['response']['alert'] = alert
-                    status['response']['status'] = 'ok'
-                    total = 1
-                else:
-                    status['response']['alert'] = None
-                    status['response']['status'] = 'not found'
-                    total = 0
-
-                diff = time.time() - start
-                status['response']['time'] = "%.3f" % diff
-                status['response']['total'] = total
-                status['response']['localTime'] = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
-
-                diff = int(diff * 1000) # XXX - management status needs time in milliseconds
-                mgmt.update(
-                    { "group": "requests", "name": "simple_get", "type": "counter", "title": "Simple GET requests", "description": "Requests to the alert status API" },
-                    { '$inc': { "count": 1, "totalTime": diff}},
-                    True)
-
-            elif os.environ['REQUEST_METHOD'] == 'PUT':
-
-                logging.info('PUT %s', cgiform.getvalue("id"))
-                form = dict()
-                update = dict()
-                for field in cgiform.keys():
-                    if field == 'id':
-                        form['_id'] = cgiform.getvalue("id")
-                    else:
-                        update[field] = cgiform.getvalue(field)
-
-                logging.debug('MongoDB MODIFY -> alerts.update(%s { $set: %s })', form, update)
-                status['response']['status'] = alerts.update(form, { '$set': update })
-
-                diff = time.time() - start
-                status['response']['time'] = "%.3f" % diff
-                status['response']['localTime'] = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
-
-                diff = int(diff * 1000) # XXX - management status needs time in milliseconds
-                mgmt.update(
-                    { "group": "requests", "name": "update", "type": "timer", "title": "PUT requests", "description": "Requests to update alerts via the API" },
-                    { '$inc': { "count": 1, "totalTime": diff}},
-                    True)
-
-            # elif os.environ['REQUEST_METHOD'] == 'DELETE':
-            elif os.environ['REQUEST_METHOD'] == 'POST' and cgiform.getvalue("_method") == 'delete':
-
-                logging.info('DELETE %s', cgiform.getvalue("id"))
-                form = dict()
-                form['_id'] = cgiform.getvalue("id")
-                
-                logging.debug('MongoDB DELETE -> alerts.remove(%s)', form)
-                status['response']['status'] = alerts.remove(form, safe=True)
-
-                diff = time.time() - start
-                status['response']['time'] = "%.3f" % diff
-                status['response']['localTime'] = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
-
-                diff = int(diff * 1000) # XXX - management status needs time in milliseconds
-                mgmt.update(
-                    { "group": "requests", "name": "delete", "type": "timer", "title": "DELETE requests", "description": "Requests to delete alerts via the API" },
-                    { '$inc': { "count": 1, "totalTime": diff}},
-                    True)
-
-        if os.environ['REQUEST_URI'].startswith('/alerta/api/v1/alerts?'):
-
-            # REQUEST_URI: /alerta/api/v1/alerts?environment=PROD&service=R2&sort-by=lastReceiveTime
-            # QUERY_STRING: environment=PROD&service=R2&sort-by=lastReceiveTime
-
-            queryStr = os.environ['QUERY_STRING']
-            form = dict([queryParam.split('=') for queryParam in queryStr.split('&')])
-
-            if 'callback' in form:
-                callback = form['callback']
-                del form['callback']
-            if 'hide-alert-details' in form:
-                hide_details = form['hide-alert-details'] == 'true'
-                del form['hide-alert-details']
-            else:
-                hide_details = False
-            if '_' in form:
-                del form['_']
-            if 'id' in form:
-                form['_id'] = form['id']
-                del form['id']
-
-            sortby = list()
-            if 'sort-by' in form:
-                for s in form['sort-by'].split('+'):
-                    sortby.append((s,-1)) # assume descending ie for dates that would be newest first
-                del form['sort-by']
-                logging.info('%s', json.dumps(sortby))
-
-            logging.info('%s', json.dumps(form))
-    
-            alertDetails = list()
-            logging.debug('MongoDB -> alerts.find(%s, sort=%s)', form, sortby)
-            for alert in alerts.find(form, sort=sortby):
-                if not hide_details:
-                    alert['id'] = alert['_id']
-                    del alert['_id']
-                    alertDetails.append(alert)
-
-            form['severity'] = 'CRITICAL'
-            critical = alerts.find(form).count()
-            form['severity'] = 'MAJOR'
-            major = alerts.find(form).count()
-            form['severity'] = 'MINOR'
-            minor = alerts.find(form).count()
-            form['severity'] = 'WARNING'
-            warning = alerts.find(form).count()
-            form['severity'] = 'NORMAL'
-            normal = alerts.find(form).count()
-            form['severity'] = 'INFORM'
-            inform = alerts.find(form).count()
-            form['severity'] = 'DEBUG'
-            debug = alerts.find(form).count()
-
-            sev = { 'critical': critical,
-                    'major': major,
-                    'minor': minor,
-                    'warning': warning,
-                    'normal': normal,
-                    'inform': inform,
-                    'debug': debug
-            }
-            logging.debug('severityCounts %s', sev)
-
-            a = { 'severityCounts': sev , 'alertDetails': list(alertDetails) }
-            status['response']['alerts'] = list()
-            status['response']['alerts'].append(a)
-    
-            diff = time.time() - start
+        logging.info('MongoDB GET -> alerts.find_one(%s)', query)
+        alert = alerts.find_one(query)
+        if alert:
+            alert['id'] = alert['_id']
+            del alert['_id']
+            status['response']['alert'] = alert
             status['response']['status'] = 'ok'
-            status['response']['time'] = "%.3f" % diff
-            status['response']['total'] = critical + major + minor + warning + normal + inform + debug
-            status['response']['localTime'] = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
-    
-            diff = int(diff * 1000) # XXX - management status needs time in milliseconds
-            mgmt.update(
-                { "group": "requests", "name": "complex_get", "type": "timer", "title": "Complex GET requests", "description": "Requests to the alert status API" },
-                { '$inc': { "count": 1, "totalTime": diff}},
-                True)
+            total = 1
+        else:
+            status['response']['alert'] = None
+            status['response']['status'] = 'not found'
+            total = 0
 
-        content = json.dumps(status, cls=DateEncoder)
-        if callback is not None:
-            content = '%s(%s);' % (callback, content)
+        diff = time.time() - start
+        status['response']['time'] = "%.3f" % diff
+        status['response']['total'] = total
+        status['response']['localTime'] = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
 
-        # logging.debug('API >>> %s', content)
+        diff = int(diff * 1000) # management status needs time in milliseconds
+        mgmt.update(
+            { "group": "requests", "name": "simple_get", "type": "counter", "title": "Simple GET requests", "description": "Requests to the alert status API" },
+            { '$inc': { "count": 1, "totalTime": diff}},
+            True)
 
-        print "Content-Type: application/javascript; charset=utf-8"
-        print "Content-Length: %s" % len(content)
-        print "Expires: -1"
-        print "Cache-Control: no-cache"
-        print "Pragma: no-cache"
-        print ""
-        print content
+    m = re.search(r'GET /alerta/api/v1/alerts$', request)  # hide-alert-details, sort-by 
+    if m:
+        logging.info('form %s' % form)
+
+        query = dict()
+        for field in form:
+            if field in ['callback', '_', 'sort-by', 'hide-alert-details']:
+                continue
+            if field == 'id':
+                query['_id'] = dict()
+                query['_id']['$in'] = form['id']
+            else:
+                query[field] = dict()
+                query[field]['$in'] = form[field]
+
+        if 'hide-alert-details' in form:
+            hide_details = form['hide-alert-details'][0] == 'true'
+        else:
+            hide_details = False
+
+        sortby = list()
+        if 'sort-by' in form:
+            for s in form['sort-by']:
+                sortby.append((s,-1)) # sort by newest first
+
+        # Init severity counts
+        critical = 0
+        major = 0
+        minor = 0
+        warning = 0
+        normal = 0
+        inform = 0
+        debug = 0
+
+        alertDetails = list()
+        logging.info('MongoDB GET all -> alerts.find(%s, sort=%s)', query, sortby)
+        for alert in alerts.find(query, sort=sortby):
+            if not hide_details:
+                alert['id'] = alert['_id']
+                del alert['_id']
+                alertDetails.append(alert)
+            if alert['severity'] == 'CRITICAL':
+                critical += 1
+            elif alert['severity'] == 'MAJOR':
+                major += 1
+            elif alert['severity'] == 'MINOR':
+                minor += 1
+            elif alert['severity'] == 'WARNING':
+                warning += 1
+            elif alert['severity'] == 'NORMAL':
+                normal += 1
+            elif alert['severity'] == 'INFORM':
+                inform += 1
+            elif alert['severity'] == 'DEBUG':
+                debug += 1
+
+        sev = { 'critical': critical,
+                'major': major,
+                'minor': minor,
+                'warning': warning,
+                'normal': normal,
+                'inform': inform,
+                'debug': debug
+        }
+        logging.info('severityCounts %s', sev)
+
+        a = { 'severityCounts': sev , 'alertDetails': list(alertDetails) }
+        status['response']['alerts'] = list()
+        status['response']['alerts'].append(a)
+
+        diff = time.time() - start
+        status['response']['status'] = 'ok'
+        status['response']['time'] = "%.3f" % diff
+        status['response']['total'] = critical + major + minor + warning + normal + inform + debug
+        status['response']['localTime'] = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+
+        diff = int(diff * 1000) # management status needs time in milliseconds
+        mgmt.update(
+            { "group": "requests", "name": "complex_get", "type": "timer", "title": "Complex GET requests", "description": "Requests to the alert status API" },
+            { '$inc': { "count": 1, "totalTime": diff}},
+            True)
+
+    m = re.search(r'PUT /alerta/api/v1/alerts/alert/(?P<id>\S+)$', request)
+    if m:
+        query['_id'] = m.group('id')
+        update = data
+
+        logging.info('MongoDB MODIFY -> alerts.update(%s, { $set: %s })', query, update)
+        error = alerts.update(query, { '$set': update }, safe=True)
+        if error['ok'] == 1:
+            status['response']['status'] = 'ok'
+
+        diff = time.time() - start
+        status['response']['time'] = "%.3f" % diff
+        status['response']['localTime'] = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+
+        diff = int(diff * 1000) # management status needs time in milliseconds
+        mgmt.update(
+            { "group": "requests", "name": "update", "type": "timer", "title": "PUT requests", "description": "Requests to update alerts via the API" },
+            { '$inc': { "count": 1, "totalTime": diff}},
+            True)
+
+    m = re.search(r'DELETE /alerta/api/v1/alerts/alert/(?P<id>\S+)$', request)
+    if m:
+        query['_id'] = m.group('id')
+
+        logging.info('MongoDB DELETE -> alerts.remove(%s)', query)
+        error = alerts.remove(query, safe=True)
+        if error['ok'] == 1:
+            status['response']['status'] = 'ok'
+
+        diff = time.time() - start
+        status['response']['time'] = "%.3f" % diff
+        status['response']['localTime'] = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+
+        diff = int(diff * 1000) # management status needs time in milliseconds
+        mgmt.update(
+            { "group": "requests", "name": "delete", "type": "timer", "title": "DELETE requests", "description": "Requests to delete alerts via the API" },
+            { '$inc': { "count": 1, "totalTime": diff}},
+            True)
+
+    if status['response']['status'] == None:
+
+        logging.error('Failed request %s', request)
+
+        diff = time.time() - start
+        status['response']['time'] = "%.3f" % diff
+        status['response']['status'] = error
+        status['response']['localTime'] = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+
+        diff = int(diff * 1000)
+        mgmt.update(
+            { "group": "requests", "name": "bad", "type": "timer", "title": "Bad requests", "description": "Failed requests to the API" },
+            { '$inc': { "count": 1, "totalTime": diff}},
+            True)
+
+    content = json.dumps(status, cls=DateEncoder)
+    if 'callback' in form:
+        content = '%s(%s);' % (form['callback'][0], content)
+
+    print "Content-Type: application/javascript; charset=utf-8"
+    print "Content-Length: %s" % len(content)
+    print "Expires: -1"
+    print "Cache-Control: no-cache"
+    print "Pragma: no-cache"
+    print ""
+    print content
+
+    logging.info('Request %s completed in %sms', request, diff)
 
 if __name__ == '__main__':
     main()
