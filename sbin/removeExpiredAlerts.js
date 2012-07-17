906f8f2f5c01d0a57e72f245ea08018e8844e68a
Nick Satterly <nick.satterly@guardian.co.uk>
2012-07-17 17:14:11 +0100

    Fix history update to expired alerts cron

--- sbin/removeExpiredAlerts.js
+++ sbin/removeExpiredAlerts.js
@@ -1,7 +1,7 @@
 // To mark expired and old alerts in the alerta database run this script from cron like so:
 // * * * * * /usr/bin/mongo --quiet monitoring /opt/alerta/sbin/removeExpiredAlerts.js
 now = new Date();
-db.alerts.update({ 'status': 'OPEN', 'expireTime': { $lt: now }}, { $set: {'status': 'EXPIRED' });
+db.alerts.update({ status: 'OPEN', expireTime: { $lt: now }}, { $set: { status: 'EXPIRED' }, $push: { history: {status: 'EXPIRED', updateTime: now }}});
 
 ago = new Date(new Date() - 2*60*60*1000);
-db.alerts.update({ 'status': 'CLOSED', 'lastReceiveTime': { $lt: ago }}, { $set: {'status': 'DELETED' }} );
+db.alerts.update({ status: 'CLOSED', lastReceiveTime: { $lt: ago }}, { $set: { status: 'DELETED' }, $push: { history: {status: 'EXPIRED', updateTime: now }}});
