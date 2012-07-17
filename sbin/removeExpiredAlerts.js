a376a3a15a061a2b2208a15153f1055c833b069e
Nick Satterly <nick.satterly@guardian.co.uk>
2012-07-17 14:40:16 +0100

    Change status of EXPIRED and DELETED alerts

--- sbin/removeExpiredAlerts.js
+++ sbin/removeExpiredAlerts.js
@@ -1,4 +1,7 @@
-// To remove expired alerts from the alerta database run this script from cron like so: 
+// To mark expired and old alerts in the alerta database run this script from cron like so:
 // * * * * * /usr/bin/mongo --quiet monitoring /opt/alerta/sbin/removeExpiredAlerts.js
 now = new Date();
-db.alerts.remove({ 'status': 'CLOSED', 'expireTime': { $lt: now }});
+db.alerts.update({ 'status': 'OPEN', 'expireTime': { $lt: now }}, { $set: {'status': 'EXPIRED' });
+
+ago = new Date(new Date() - 2*60*60*1000);
+db.alerts.update({ 'status': 'CLOSED', 'lastReceiveTime': { $lt: ago }}, { $set: {'status': 'DELETED' }} );
