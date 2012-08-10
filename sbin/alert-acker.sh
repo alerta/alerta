ccc4e614f27a9bd882d0412fa6fb565bacd23db0
Nick Satterly <nick.satterly@guardian.co.uk>
2012-08-10 09:17:16 +0100

    Minor fix to alert-acker

--- sbin/alert-acker.sh
+++ sbin/alert-acker.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
 if [ "$1" = "" ]; then
     echo "Must supply alert ID to acknowledge"
@@ -6,5 +6,5 @@ else
     ALERTID=$1
 fi
 
-curl -XPUT http://${2:-'monitoring.gudev.gnl'}/alerta/api/v1/alerts/alert/${ALERTID} -d '{ "status": "ACK" }'
+curl -XPUT http://${2:-'monitoring.guprod.gnl'}/alerta/api/v1/alerts/alert/${ALERTID} -d '{ "status": "ACK" }'
 echo
