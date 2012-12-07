35111353ab8d8cae320b49520fe693114fed160f
Nick Satterly <nick.satterly@guardian.co.uk>
2012-12-07 13:03:47 +0000

    Add more service lookups for Deploys

--- bin/parsers/DeploysServiceLookup.py
+++ bin/parsers/DeploysServiceLookup.py
@@ -1,21 +1,29 @@
 
-if 'r2' in alert['resource'].lower():
+if alert['resource'].startswith('R1'):
+    alert['service'] = [ 'R1' ]
+elif alert['resource'].startswith('R2'):
     alert['service'] = [ 'R2' ]
 elif 'content-api' in alert['resource'].lower():
     alert['service'] = [ 'ContentAPI' ]
+elif alert['resource'].startswith('frontend'):
+    alert['service'] = [ 'Frontend' ]
 elif 'flexible' in alert['resource'].lower():
     alert['service'] = [ 'FlexibleContent' ]
-elif 'frontend' in alert['resource'].lower():
-    alert['service'] = [ 'Frontend' ]
-elif 'mobile' in alert['resource'].lower():
-    alert['service'] = [ 'Mobile' ]
-elif 'android' in alert['resource'].lower():
-    alert['service'] = [ 'Mobile' ]
-elif 'ios' in alert['resource'].lower():
-    alert['service'] = [ 'Mobile' ]
-elif 'identity' in alert['resource'].lower():
+elif alert['resource'].startswith('Identity'):
     alert['service'] = [ 'Identity' ]
-elif 'microapps' in alert['resource'].lower():
+elif alert['resource'].startswith('Mobile'):
+    alert['service'] = [ 'Mobile' ]
+elif alert['resource'].startswith('Android'):
+    alert['service'] = [ 'Mobile' ]
+elif alert['resource'].startswith('iOS'):
+    alert['service'] = [ 'Mobile' ]
+elif alert['resource'].startswith('Soulmates'):
+    alert['service'] = [ 'Soulmates' ]
+elif alert['resource'].startswith('Microapps'):
     alert['service'] = [ 'MicroApp' ]
+elif alert['resource'].startswith('Mutualisation'):
+    alert['service'] = [ 'Mutualisation' ]
+elif alert['resource'].startswith('Ophan'):
+    alert['service'] = [ 'Ophan' ]
 else:
     alert['service'] = [ 'Unknown' ]
