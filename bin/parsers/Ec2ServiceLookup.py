a347b41d384a9ef9a4bd28027c07f44e0ad3d406
Nick Satterly <nick.satterly@guardian.co.uk>
2012-12-17 17:09:22 +0000

    Update Ec2ServiceLookup transformer

--- bin/parsers/Ec2ServiceLookup.py
+++ bin/parsers/Ec2ServiceLookup.py
@@ -1,8 +1,20 @@
 # contentapimq -> ContentAPI
 if any(tag.startswith('cluster:contentapimq') for tag in alert['tags']):
     alert['service'] = [ 'ContentAPI' ]
+# frontend-* -> Frontend
+elif any(tag.startswith('cluster:frontend') for tag in alert['tags']):
+    alert['service'] = [ 'Frontend' ]
+# soulmates-* -> Soulmates
+# geolocation -> Soulmates
+elif any(tag.startswith('cluster:soulmates') for tag in alert['tags']):
+    alert['service'] = [ 'Soulmates' ]
+elif any(tag.startswith('cluster:geolocation-api') for tag in alert['tags']):
+    alert['service'] = [ 'Soulmates' ]
 # discussion-app -> Discussion
+# renderer -> Discussion
 elif any(tag.startswith('cluster:discussion') for tag in alert['tags']):
+    alert['service'] = [ 'Discussion' ]
+elif any(tag.startswith('cluster:renderer') for tag in alert['tags']):
     alert['service'] = [ 'Discussion' ]
 # mobile-aggregator -> Mobile
 elif any(tag.startswith('cluster:mobile') for tag in alert['tags']):
@@ -11,10 +23,13 @@ elif any(tag.startswith('cluster:mobile') for tag in alert['tags']):
 elif any(tag.startswith('cluster:content-authorisation') for tag in alert['tags']):
     alert['service'] = [ 'Mobile' ]
 # ios-purchases -> Mobile
-elif any(tag.startswith('cluster:ios-purchases') for tag in alert['tags']):
+elif any(tag.startswith('cluster:ios_purchases') for tag in alert['tags']):
     alert['service'] = [ 'Mobile' ]
 # ipad-ad-preview -> Mobile
-elif any(tag.startswith('cluster:ipad-ad-preview') for tag in alert['tags']):
+elif any(tag.startswith('cluster:ipad_ad_preview') for tag in alert['tags']):
+    alert['service'] = [ 'Mobile' ]
+# pushy_galore -> Mobile
+elif any(tag.startswith('cluster:pushy_galore') for tag in alert['tags']):
     alert['service'] = [ 'Mobile' ]
 # mongo-cluster -> SharedSvcs
 elif any(tag.startswith('cluster:mongo-cluster') for tag in alert['tags']):
@@ -39,5 +54,14 @@ elif any(tag.startswith('cluster:gov-spending') for tag in alert['tags']):
 # interactive-traffic-stats -> Other
 elif any(tag.startswith('cluster:interactive') for tag in alert['tags']):
     alert['service'] = [ 'Other' ]
+# ganglia-* -> SharedSvcs
+# puppet-* -> SharedSvcs
+# zabbix-* -> SharedSvcs
+elif any(tag.startswith('cluster:ganglia') for tag in alert['tags']):
+    alert['service'] = [ 'SharedSvcs' ]
+elif any(tag.startswith('cluster:puppet') for tag in alert['tags']):
+    alert['service'] = [ 'SharedSvcs' ]
+elif any(tag.startswith('cluster:zabbix') for tag in alert['tags']):
+    alert['service'] = [ 'SharedSvcs' ]
 else:
     alert['service'] = [ 'Unknown' ]
