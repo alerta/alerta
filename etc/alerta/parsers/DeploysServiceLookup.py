b169be652efdc514e557c53841afabb30189f4d9
Nick Satterly <nick.satterly@guardian.co.uk>
2013-03-25 11:04:09 +0000

    sort out parsers

--- etc/alerta/parsers/DeploysServiceLookup.py
+++ etc/alerta/parsers/DeploysServiceLookup.py
@@ -1,32 +1,46 @@
 
-if alert['resource'].startswith('R1'):
-    alert['service'] = [ 'R1' ]
-elif alert['resource'].startswith('R2'):
-    alert['service'] = [ 'R2' ]
-elif 'content-app' in alert['resource'].lower():
-    alert['service'] = [ 'ContentAPI' ]
-elif alert['resource'].startswith('frontend'):
-    alert['service'] = [ 'Frontend' ]
+if resource.startswith('R1'):
+    service = ['R1']
+    
+elif resource.startswith('R2'):
+    service = ['R2']
+    
+elif 'content-app' in resource.lower():
+    service = ['ContentAPI']
+    
+elif resource.startswith('frontend'):
+    service = ['Frontend']
+    
     if alert['event'] == 'DeployFailed':
-        alert['severity'] = 'CRITICAL'
+        alert['severity'] = severity.CRITICAL
         alert['tags'].append('email:frontend')
-elif 'flexible' in alert['resource'].lower():
-    alert['service'] = [ 'FlexibleContent' ]
-elif alert['resource'].startswith('Identity'):
-    alert['service'] = [ 'Identity' ]
-elif alert['resource'].startswith('Mobile'):
-    alert['service'] = [ 'Mobile' ]
-elif alert['resource'].startswith('Android'):
-    alert['service'] = [ 'Mobile' ]
-elif alert['resource'].startswith('iOS'):
-    alert['service'] = [ 'Mobile' ]
-elif alert['resource'].startswith('Soulmates'):
-    alert['service'] = [ 'Soulmates' ]
-elif alert['resource'].startswith('Microapps'):
-    alert['service'] = [ 'MicroApp' ]
-elif alert['resource'].startswith('Mutualisation'):
-    alert['service'] = [ 'Mutualisation' ]
-elif alert['resource'].startswith('Ophan'):
-    alert['service'] = [ 'Ophan' ]
+        
+elif 'flexible' in resource.lower():
+    service = ['FlexibleContent']
+    
+elif resource.startswith('Identity'):
+    service = ['Identity']
+    
+elif resource.startswith('Mobile'):
+    service = ['Mobile']
+    
+elif resource.startswith('Android'):
+    service = ['Mobile']
+    
+elif resource.startswith('iOS'):
+    service = ['Mobile']
+    
+elif resource.startswith('Soulmates'):
+    service = ['Soulmates']
+    
+elif resource.startswith('Microapps'):
+    service = ['MicroApp']
+    
+elif resource.startswith('Mutualisation'):
+    service = ['Mutualisation']
+    
+elif resource.startswith('Ophan'):
+    service = ['Ophan']
+    
 else:
-    alert['service'] = [ 'Unknown' ]
+    service = ['Unknown']
--- etc/alerta/parsers/Ec2ServiceLookup.py
+++ etc/alerta/parsers/Ec2ServiceLookup.py
@@ -1,69 +1,87 @@
+
 # contentapimq -> ContentAPI
-if any(tag.startswith('cluster:contentapimq') for tag in alert['tags']):
-    alert['service'] = [ 'ContentAPI' ]
+if any(tag.startswith('cluster:contentapimq') for tag in tags):
+    service = ['ContentAPI']
+
 # frontend-* -> Frontend
-elif any(tag.startswith('cluster:frontend') for tag in alert['tags']):
-    alert['service'] = [ 'Frontend' ]
+elif any(tag.startswith('cluster:frontend') for tag in tags):
+    service = ['Frontend']
+
 # soulmates-* -> Soulmates
 # geolocation -> Soulmates
-elif any(tag.startswith('cluster:soulmates') for tag in alert['tags']):
-    alert['service'] = [ 'Soulmates' ]
-elif any(tag.startswith('cluster:geolocation-app') for tag in alert['tags']):
-    alert['service'] = [ 'Soulmates' ]
+elif any(tag.startswith('cluster:soulmates') for tag in tags):
+    service = ['Soulmates']
+elif any(tag.startswith('cluster:geolocation-app') for tag in tags):
+    service = ['Soulmates']
+
 # discussion-app -> Discussion
 # renderer -> Discussion
-elif any(tag.startswith('cluster:discussion') for tag in alert['tags']):
-    alert['service'] = [ 'Discussion' ]
-elif any(tag.startswith('cluster:renderer') for tag in alert['tags']):
-    alert['service'] = [ 'Discussion' ]
+elif any(tag.startswith('cluster:discussion') for tag in tags):
+    service = ['Discussion']
+elif any(tag.startswith('cluster:renderer') for tag in tags):
+    service = ['Discussion']
+
 # mobile-aggregator -> Mobile
-elif any(tag.startswith('cluster:mobile') for tag in alert['tags']):
-    alert['service'] = [ 'Mobile' ]
+elif any(tag.startswith('cluster:mobile') for tag in tags):
+    service = ['Mobile']
+
 # content-authorisation -> Mobile
-elif any(tag.startswith('cluster:content-authorisation') for tag in alert['tags']):
-    alert['service'] = [ 'Mobile' ]
+elif any(tag.startswith('cluster:content-authorisation') for tag in tags):
+    service = ['Mobile']
+
 # ios-purchases -> Mobile
-elif any(tag.startswith('cluster:ios_purchases') for tag in alert['tags']):
-    alert['service'] = [ 'Mobile' ]
+elif any(tag.startswith('cluster:ios_purchases') for tag in tags):
+    service = ['Mobile']
+
 # ipad-ad-preview -> Mobile
-elif any(tag.startswith('cluster:ipad_ad_preview') for tag in alert['tags']):
-    alert['service'] = [ 'Mobile' ]
+elif any(tag.startswith('cluster:ipad_ad_preview') for tag in tags):
+    service = ['Mobile']
+
 # pushy_galore -> Mobile
-elif any(tag.startswith('cluster:pushy_galore') for tag in alert['tags']):
-    alert['service'] = [ 'Mobile' ]
+elif any(tag.startswith('cluster:pushy_galore') for tag in tags):
+    service = ['Mobile']
+
 # mongo-cluster -> SharedSvcs
-elif any(tag.startswith('cluster:mongo-cluster') for tag in alert['tags']):
-    alert['service'] = [ 'SharedSvcs' ]
+elif any(tag.startswith('cluster:mongo-cluster') for tag in tags):
+    service = ['SharedSvcs']
+
 # outboundproxy -> SharedSvcs
-elif any(tag.startswith('cluster:outboundproxy') for tag in alert['tags']):
-    alert['service'] = [ 'SharedSvcs' ]
+elif any(tag.startswith('cluster:outboundproxy') for tag in tags):
+    service = ['SharedSvcs']
+
 # arts-books -> Mutualisation
 # arts-music -> Mutualisation
-elif any(tag.startswith('cluster:arts') for tag in alert['tags']):
-    alert['service'] = [ 'Mutualisation' ]
+elif any(tag.startswith('cluster:arts') for tag in tags):
+    service = [ 'Mutualisation' ]
+
 # lists-service -> Mutualisation
-elif any(tag.startswith('cluster:lists-service') for tag in alert['tags']):
-    alert['service'] = [ 'Mutualisation' ]
+elif any(tag.startswith('cluster:lists-service') for tag in tags):
+    service = ['Mutualisation']
+
 # cutswatch-db -> Other
 # cutswatch-frontend -> Other
-elif any(tag.startswith('cluster:cutswatch') for tag in alert['tags']):
-    alert['service'] = [ 'Other' ]
+elif any(tag.startswith('cluster:cutswatch') for tag in tags):
+    service = ['Other']
+
 # gov-spending -> Other
-elif any(tag.startswith('cluster:gov-spending') for tag in alert['tags']):
-    alert['service'] = [ 'Other' ]
+elif any(tag.startswith('cluster:gov-spending') for tag in tags):
+    service = ['Other']
+
 # interactive-traffic-stats -> Other
-elif any(tag.startswith('cluster:interactive') for tag in alert['tags']):
-    alert['service'] = [ 'Other' ]
+elif any(tag.startswith('cluster:interactive') for tag in tags):
+    service = ['Other']
+
 # ganglia-* -> SharedSvcs
 # puppet-* -> SharedSvcs
 # zabbix-* -> SharedSvcs
-elif any(tag.startswith('cluster:ganglia') for tag in alert['tags']):
-    alert['service'] = [ 'SharedSvcs' ]
-elif any(tag.startswith('cluster:puppet') for tag in alert['tags']):
-    alert['service'] = [ 'SharedSvcs' ]
-elif any(tag.startswith('cluster:zabbix') for tag in alert['tags']):
-    alert['service'] = [ 'SharedSvcs' ]
+elif any(tag.startswith('cluster:ganglia') for tag in tags):
+    service = ['SharedSvcs']
+elif any(tag.startswith('cluster:puppet') for tag in tags):
+    service = ['SharedSvcs']
+elif any(tag.startswith('cluster:zabbix') for tag in tags):
+    service = ['SharedSvcs']
 else:
-    alert['service'] = [ 'Unknown' ]
+    service = ['Unknown']
 
-alert['summary'] = '%s - %s %s is %s on %s %s' % (','.join(alert['environment']), alert['severity'], alert['event'], alert['value'], ','.join(alert['service']), alert['resource'])
+# Redo summary because service has changed
+summary = '%s - %s %s is %s on %s %s' % (','.join(environment), severity, event, value, ','.join(service), resource)
