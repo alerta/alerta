10b5bb2e14890088aa27646f18a909e00ccbabbd
Nick Satterly <nick.satterly@guardian.co.uk>
2013-04-10 00:08:00 +0100

    Update tests

--- tests/test_alert.py
+++ tests/test_alert.py
@@ -1,7 +1,17 @@
 
+import os
+import sys
 import unittest
 
 import datetime
+
+# If ../alerta/__init__.py exists, add ../ to Python search path, so that
+# it will override what happens to be installed in /usr/(local/)lib/python...
+possible_topdir = os.path.normpath(os.path.join(os.path.abspath(sys.argv[0]),
+                                                os.pardir,
+                                                os.pardir))
+if os.path.exists(os.path.join(possible_topdir, 'alerta', '__init__.py')):
+    sys.path.insert(0, possible_topdir)
 
 from alerta.common.alert import Alert, status_code, severity_code
 
@@ -120,3 +130,6 @@ class TestAlert(unittest.TestCase):
         self.assertEquals(alert.more_info, self.MORE_INFO)
         self.assertEquals(alert.graph_urls, self.GRAPH_URLS)
         self.assertEquals(alert.history, self.HISTORY)
+
+if __name__ == '__main__':
+    unittest.main()
