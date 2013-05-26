d81022bd3f9237e8bfd6769396ec04a52312c10a
Nick Satterly <nick.satterly@guardian.co.uk>
2013-05-26 18:34:04 +0100

    add carbon tcp metric send support

--- alerta/common/graphite.py
+++ alerta/common/graphite.py
@@ -28,8 +28,18 @@ class Carbon(object):
 
         LOG.debug('Carbon setup to send %s packets to %s:%s', self.protocol, self.host, self.port)
 
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.hostport = (self.host, int(self.port))
+
+        if protocol == 'udp':
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        else:
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            try:
+                self.socket.connect(self.hostport)
+                self._connected = True
+            except socket.error:
+                self._connected = False
+                LOG.warning('Carbon server %s not responding on TCP port %s', self.host, self.port)
 
     def metric_send(self, name, value, timestamp=None):
 
@@ -41,6 +51,35 @@ class Carbon(object):
             timestamp = int(time.time())
 
         LOG.debug('carbon name=%s, value=%s, timestamp=%s', name, value, timestamp)
-        count = self.socket.sendto('%s %s %s\n' % (name, value, timestamp), self.hostport)
-        LOG.debug('Sent %s data packets', count)
+
+        if self.protocol == 'udp':
+            count = self.socket.sendto('%s %s %s\n' % (name, value, timestamp), self.hostport)
+            LOG.debug('Sent %s UDP metric packets', count)
+        else:
+            if not self._connected:
+                LOG.info('Attempting reconnect to Carbon server %s:%s', self.host, self.port)
+                try:
+                    self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    self.socket.connect(self.hostport)
+                    self._connected = True
+                    LOG.info('Reconnected to Carbon server %s on TCP port %s', self.host, self.port)
+                except socket.error:
+                    LOG.warning('Carbon server %s not responding to TCP port %s', self.host, self.port)
+                    self._connected = False
+                    return
+
+            if self._connected:
+                try:
+                    self.socket.sendall('%s %s %s\n' % (name, value, timestamp))
+                    LOG.debug('Sent all TCP metric data')
+                except socket.error, e:
+                    self._connected = False
+                    LOG.warning('Carbon server %s not responding on TCP port %s', self.host, self.port)
+
+    def shutdown(self):
+
+        if self.protocol == 'tcp':
+            self.socket.close()
+
+
 
