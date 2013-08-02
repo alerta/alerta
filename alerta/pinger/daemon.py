bd5b00896a3cd6f29bd9d1d6000c73fb995af07e
Nick Satterly <nick.satterly@guardian.co.uk>
2013-08-02 16:15:14 +0100

    make ping alerts serviceAlert type

--- alerta/pinger/daemon.py
+++ alerta/pinger/daemon.py
@@ -17,7 +17,7 @@ from alerta.common.daemon import Daemon
 from alerta.common.dedup import DeDup
 from alerta.common.graphite import Carbon, StatsD
 
-Version = '2.0.13'
+Version = '2.0.14'
 
 LOG = logging.getLogger(__name__)
 CONF = config.CONF
@@ -140,7 +140,7 @@ class WorkerThread(threading.Thread):
                 environment=environment,
                 service=service,
                 text=text,
-                event_type='exceptionAlert',
+                event_type='serviceAlert',
                 tags=None,
                 timeout=timeout,
                 threshold_info=threshold_info,
