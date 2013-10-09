eb6576ac6a30e5ba8f8e46641e90eab76f1a9ecf
Nick Satterly <nick.satterly@guardian.co.uk>
2013-10-09 23:34:18 +0100

    enforce UTC/Z iso8601 datetime format

--- alerta/common/utils.py
+++ alerta/common/utils.py
@@ -63,7 +63,7 @@ class DateEncoder(json.JSONEncoder):
     def default(self, obj):
 
         if isinstance(obj, (datetime.date, datetime.datetime)):
-            return obj.replace(microsecond=0).isoformat() + ".%03dZ" % (obj.microsecond // 1000)
+            return obj.replace(microsecond=0).strftime('%Y-%m-%dT%H:%M:%S') + ".%03dZ" % (obj.microsecond // 1000)
         else:
             return json.JSONEncoder.default(self, obj)
 
