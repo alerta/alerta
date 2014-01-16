e7b23cd8e07fa068f9c1fb877177d8b8de4cf2bc
Nick Satterly <nick.satterly@guardian.co.uk>
2014-01-16 01:33:18 +0000

    minor fixes

--- alerta/cloudwatch/daemon.py
+++ alerta/cloudwatch/daemon.py
@@ -16,7 +16,7 @@ from alerta.common.dedup import DeDup
 from alerta.common.mq import Messaging, MessageHandler
 from alerta.common.graphite import StatsD
 
-Version = '2.0.0'
+Version = '2.0.1'
 
 LOG = logging.getLogger(__name__)
 CONF = config.CONF
@@ -37,8 +37,8 @@ class CloudWatchDaemon(Daemon):
     cloudwatch_opts = {
         'cloudwatch_sqs_region': 'eu-west-1',
         'cloudwatch_sqs_queue': 'alerta',
-        'cloudwatch_access_key': 'AKIAIAK2IYG7WDX5L6VA',
-        'cloudwatch_secret_key': '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'
+        'cloudwatch_access_key': '022QF06E7MXBSAMPLE',
+        'cloudwatch_secret_key': ''
     }
 
     def __init__(self, prog, **kwargs):
@@ -81,7 +81,7 @@ class CloudWatchDaemon(Daemon):
                     cloudwatchAlert = self.parse_notification(message)
                     if self.dedup.is_send(cloudwatchAlert):
                         self.mq.send(cloudwatchAlert)
-                    # q.delete_message(m)
+                    q.delete_message(m)
 
                 LOG.debug('Send heartbeat...')
                 heartbeat = Heartbeat(version=Version)
@@ -98,36 +98,13 @@ class CloudWatchDaemon(Daemon):
 
     def parse_notification(self, message):
 
-        LOG.debug('Parsing CloudWatch Notification message...')
-
-        # {
-        #   "Type" : "Notification",
-        #   "MessageId" : "29ecfcf6-7bd2-5cc9-bfdd-b7a75b3999a0",
-        #   "TopicArn" : "arn:aws:sns:eu-west-1:496780030265:alerta",
-        #   "Subject" : "ALARM: \"awselb-lb001-High-Unhealthy-Hosts\" in EU - Ireland",
-        #   "Message" : "{\"AlarmName\":\"awselb-lb001-High-Unhealthy-Hosts\",\"AlarmDescription\":\"Created from AWS Console\",\"AWSAccountId\":\"496780030265\",\"NewStateValue\":\"ALARM\",\"NewStateReason\":\"Threshold Crossed: 1 datapoint (1.0) was greater than or equal to the threshold (0.0).\",\"StateChangeTime\":\"2014-01-15T23:50:03.190+0000\",\"Region\":\"EU - Ireland\",\"OldStateValue\":\"INSUFFICIENT_DATA\",\"Trigger\":{\"MetricName\":\"UnHealthyHostCount\",\"Namespace\":\"AWS/ELB\",\"Statistic\":\"SUM\",\"Unit\":null,\"Dimensions\":[{\"name\":\"LoadBalancerName\",\"value\":\"lb001\"}],\"Period\":60,\"EvaluationPeriods\":1,\"ComparisonOperator\":\"GreaterThanOrEqualToThreshold\",\"Threshold\":0.0}}",
-        #   "Timestamp" : "2014-01-15T23:50:03.242Z",
-        #   "SignatureVersion" : "1",
-        #   "Signature" : "TlJumlRUe9ia1/wxpQwq2eBMXLFgd40057UgUDfShUn4qGYaNFNhkPvL+k6p5yZpjTT7TQUYLXQOrvphRgZSg7/+jEvw5EGOT27t1BYHqpLBolEUgg6xBZD/chWImpGJErotjvrIMaCxQR/Bxb8rYU420LsexdpWQCe4nvUr8A6i/gaevGb9YwcYQtmf/vPLXiwDMtLGAeuhnuyYy1xuOF2gKNfxhLKYb2i/+cAwJCEaBOO9hlKtyGA5R0QZ1PN4dy2Gr8VWs6p0/VUzkfX3gREEsoEk1laKaQ7868Y6Uu5FQcVQqooy3YtgkkP7XUoHxn4Fe/7uHIlfXKK+Oyv17A==",
-        #   "SigningCertURL" : "https://sns.eu-west-1.amazonaws.com/SimpleNotificationService-e372f8ca30337fdb084e8ac449342c77.pem",
-        #   "UnsubscribeURL" : "https://sns.eu-west-1.amazonaws.com/?Action=Unsubscribe&SubscriptionArn=arn:aws:sns:eu-west-1:496780030265:alerta:ddea6c9c-ee0a-4972-bb41-9f4660e9990b"
-        # }
-
-        alarm_ex = {u'NewStateReason': u'Threshold Crossed: 1 datapoint (1.0) was greater than or equal to the threshold (0.0).',
-             u'Region': u'EU - Ireland',
-             u'AlarmDescription': u'Created from AWS Console',
-             u'NewStateValue': u'ALARM',
-             u'Trigger': {u'EvaluationPeriods': 1, u'Dimensions': [{u'name': u'LoadBalancerName', u'value': u'lb001'}],
-                          u'Namespace': u'AWS/ELB', u'Period': 60, u'ComparisonOperator': u'GreaterThanOrEqualToThreshold',
-                          u'Statistic': u'SUM', u'Threshold': 0.0, u'Unit': None, u'MetricName': u'UnHealthyHostCount'},
-             u'OldStateValue': u'INSUFFICIENT_DATA',
-             u'AlarmName': u'awselb-lb001-High-Unhealthy-Hosts',
-             u'StateChangeTime': u'2014-01-15T23:50:03.190+0000',
-             u'AWSAccountId': u'496780030265'}
+        LOG.debug('Parsing CloudWatch notification message...')
 
         notification = json.loads(message)
         if 'Message' in notification:
             alarm = json.loads(notification['Message'])
+        else:
+            return
 
         # Defaults
         alertid = notification['MessageId']
