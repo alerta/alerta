0682823119801c3a581e06e22b39204949a2bc2d
Nick Satterly <nick.satterly@guardian.co.uk>
2014-03-27 22:09:58 +0000

    remove old v1 dashboard

--- dashboard/v1/includes/alerts.php
+++
@@ -1,6 +0,0 @@
-      <span id="heartbeat-alerts"></span>
-      <div id="console-alert" class="alert alert-info initially-hidden">
-        <button class="close" data-dismiss="alert" onclick="">&times;</button>
-        <strong>Warning!</strong> <span id="warning-text"></span>
-      </div>
-      </table>
