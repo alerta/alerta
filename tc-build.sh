7d6e35042f3515dca4386c966d9bd4623567ab16
Nick Satterly <nick.satterly@guardian.co.uk>
2014-03-28 14:24:24 +0000

    get teamcity to build RPMs

--- tc-build.sh
+++ tc-build.sh
@@ -1,93 +1,30 @@
 #!/bin/bash
+# Build script for teamcity to build the alerta RPMs
 
-set -e
-set -x
-
-# prep
-pushd `dirname $0` >/dev/null
-SCRIPT_PATH=`pwd`
-popd >/dev/null
-
-rm -Rf $SCRIPT_PATH/app
-rm -Rf $SCRIPT_PATH/dashboard
-rm -Rf $SCRIPT_PATH/daemons
-rm -Rf $SCRIPT_PATH/dist
-rm -Rf $SCRIPT_PATH/alerta.egg-info
-
-if [ -z "${BUILD_NUMBER}" ]; then
-    BUILD_NUMBER=DEV
+if [ -z "$BUILD_NUMBER" ]
+then
+	echo "ERROR: BUILD_NUMBER environment variable not set by TeamCity"
+	exit 1
 fi
 
-pushd $SCRIPT_PATH
+VERSION="3.0.0"
 
-echo "#Creating App virtualenv..."
-virtualenv app
-app/bin/python setup.py clean
+SCRIPT=$(readlink -f $0)
+ALERTA_VCS_ROOT=`dirname $SCRIPT`
 
-echo "#Installing dependencies"
-app/bin/pip install -r requirements.txt --upgrade
-virtualenv --relocatable app
+BUILDROOT=${ALERTA_VCS_ROOT}/rpmtarget
+rm -rf ${BUILDROOT}
+mkdir ${BUILDROOT} \
+	${BUILDROOT}/SOURCES \
+	${BUILDROOT}/SRPMS \
+	${BUILDROOT}/SPECS \
+	${BUILDROOT}/BUILD \
+	${BUILDROOT}/WORKING \
+	${BUILDROOT}/RPMS
 
-echo "#Set build number in manifest..."
-cat << EOF > alerta/build.py
-BUILD_NUMBER = '${BUILD_NUMBER}'
-EOF
+git archive --format=tar --prefix="alerta-${version}/" HEAD | gzip > ${BUILDROOT}/SOURCES/alerta-${version}.tar.gz
 
-echo "#Build distribution zip..."
-app/bin/python setup.py sdist --formats=zip
+rpmbuild --define "version ${VERSION}" --define "release ${BUILD_NUMBER}" --define "_topdir ${BUILDROOT}" -bb ${ALERTA_VCS_ROOT}/alerta.spec || exit 1
 
-mkdir -p dist/packages/app
-unzip $SCRIPT_PATH/dist/alerta*.zip -d $SCRIPT_PATH/dist/packages/app
-
-BUILD_DIR=`ls -1 dist/packages/app`
-cp -r app dist/packages/app/${BUILD_DIR}
-
-
-echo "#Creating Dashboard virtualenv..."
-virtualenv dashboard
-dashboard/bin/python setup.py clean
-
-echo "#Installing dependencies..."
-dashboard/bin/pip install -r requirements.txt --upgrade
-virtualenv --relocatable dashboard
-
-echo "Build distribution zip"
-dashboard/bin/python setup.py sdist --formats=zip
-
-mkdir -p dist/packages/dashboard
-unzip $SCRIPT_PATH/dist/alerta*.zip -d $SCRIPT_PATH/dist/packages/dashboard
-
-BUILD_DIR=`ls -1 dist/packages/dashboard`
-cp -r dashboard dist/packages/dashboard/${BUILD_DIR}
-
-
-echo "#Creating Daemon virtualenv..."
-virtualenv daemons
-daemons/bin/python setup.py clean
-
-echo "#Installing dependencies..."
-daemons/bin/pip install -r requirements.txt --upgrade
-virtualenv --relocatable daemons
-
-echo "Build distribution zip"
-daemons/bin/python setup.py sdist --formats=zip
-
-echo "#Create daemons scripts"
-daemons/bin/python setup.py install
-
-mkdir -p dist/packages/daemons
-unzip $SCRIPT_PATH/dist/alerta*.zip -d $SCRIPT_PATH/dist/packages/daemons
-
-BUILD_DIR=`ls -1 dist/packages/daemons`
-cp -r daemons dist/packages/daemons/${BUILD_DIR}
-
-
-echo "#Create artifact.zip..."
-cp contrib/riffraff/deploy.json dist/
-pushd dist
-zip -r artifacts.zip deploy.json packages
-popd
-echo "#Rezipped to artifacts.zip"
-popd
-
-echo "##teamcity[publishArtifacts '$SCRIPT_PATH/dist/artifacts.zip => .']"
+echo "##teamcity[publishArtifacts '${BUILDROOT}/RPMS/noarch/alerta-${VERSION}-${BUILD_NUMBER}.noarch.rpm => .']"
+echo "##teamcity[publishArtifacts '${BUILDROOT}/RPMS/noarch/alerta-extras-${VERSION}-${BUILD_NUMBER}.noarch.rpm => .']"
