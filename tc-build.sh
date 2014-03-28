150e2501f9ed09a8f449a7fa69c3365678fa6ce8
Nick Satterly <nick.satterly@guardian.co.uk>
2014-03-28 16:49:03 +0000

    put RPMs in artifacts.zip file

--- tc-build.sh
+++ tc-build.sh
@@ -25,5 +25,6 @@ tar zcvf ${BUILDROOT}/SOURCES/alerta-${VERSION}.tar.gz --xform 's,^,alerta-'"${V
 sleep 1
 rpmbuild -v --define "version ${VERSION}" --define "release ${BUILD_NUMBER}" --define "_topdir ${BUILDROOT}" -bb ${ALERTA_VCS_ROOT}/alerta.spec || exit 1
 
-echo "##teamcity[publishArtifacts '${BUILDROOT}/RPMS/x86_64/alerta-${VERSION}-${BUILD_NUMBER}.x86_64.rpm => .']"
-echo "##teamcity[publishArtifacts '${BUILDROOT}/RPMS/x86_64/alerta-extras-${VERSION}-${BUILD_NUMBER}.x86_64.rpm => .']"
+zip -r ${BUILDROOT}/artifacts.zip contrib/riffraff/deploy.json ${BUILDROOT}/RPMS/x86_64/alerta-${VERSION}-${BUILD_NUMBER}.x86_64.rpm ${BUILDROOT}/RPMS/x86_64/alerta-extras-${VERSION}-${BUILD_NUMBER}.x86_64.rpm
+
+echo "##teamcity[publishArtifacts '${BUILDROOT}/artifacts.zip => .']"
