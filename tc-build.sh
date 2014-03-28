88f09a43e685ffe9fcb2df9a841680cdf8dca0f1
Nick Satterly <nick.satterly@guardian.co.uk>
2014-03-28 16:27:39 +0000

    sleep before building

--- tc-build.sh
+++ tc-build.sh
@@ -22,7 +22,7 @@ mkdir ${BUILDROOT} \
 	${BUILDROOT}/RPMS
 
 tar zcvf ${BUILDROOT}/SOURCES/alerta-${VERSION}.tar.gz --xform 's,^,alerta-'"${VERSION}"'/,S' * >/dev/null
-
+sleep 1
 rpmbuild -v --define "version ${VERSION}" --define "release ${BUILD_NUMBER}" --define "_topdir ${BUILDROOT}" -bb ${ALERTA_VCS_ROOT}/alerta.spec || exit 1
 
 echo "##teamcity[publishArtifacts '${BUILDROOT}/RPMS/x86_64/alerta-${VERSION}-${BUILD_NUMBER}.noarch.rpm => .']"
