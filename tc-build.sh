c0dd9f419fa5ea6a0b5e702b5a2ec3dda811e7ab
Nick Satterly <nick.satterly@guardian.co.uk>
2014-03-31 16:11:19 +0100

    add riffraff recipe for dependencies

--- tc-build.sh
+++ tc-build.sh
@@ -33,7 +33,7 @@ mkdir -p packages/alerta
 mv ${BUILDROOT}/RPMS/x86_64/alerta-${VERSION}-${BUILD_NUMBER}.x86_64.rpm packages/alerta
 mkdir -p packages/alerta-extras
 mv ${BUILDROOT}/RPMS/x86_64/alerta-extras-${VERSION}-${BUILD_NUMBER}.x86_64.rpm packages/alerta-extras
-zip -r ${ALERTA_VCS_ROOT}/artifacts.zip deploy.json packages
+zip -r ../artifacts.zip deploy.json packages
 popd
 
 # Pushlish artifact
