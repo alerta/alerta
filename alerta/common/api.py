9db1a4ae9e77304bf796b0f8abb1ca02b8042b29
Nick Satterly <nick.satterly@guardian.co.uk>
2014-04-07 20:18:27 +0100

    use same ApiClient module as client

--- alerta/common/api.py
+++ alerta/common/api.py
@@ -1,14 +1,12 @@
 
-import sys
 import json
 import urllib
 import requests
 
 from alerta.common import log as logging
 from alerta.common import config
-from alerta.common.utils import DateEncoder
 
-__version__ = '3.0.2'
+__version__ = '3.0.3'
 
 LOG = logging.getLogger(__name__)
 CONF = config.CONF
@@ -17,240 +15,134 @@ CONF = config.CONF
 class ApiClient(object):
 
     api_opts = {
-        'api_host': 'localhost',
-        'api_port': 8080,
-        'api_root': '/api',
+        'endpoint': 'http://localhost:8080'
     }
 
-    def __init__(self, host=None, port=None, root=None):
+    def __init__(self, endpoint=None):
 
         config.register_opts(ApiClient.api_opts)
 
-        self.host = host or CONF.api_host
-        self.port = port or CONF.api_port
-        self.root = root or CONF.api_root
+        self.endpoint = endpoint or CONF.endpoint
+
+    def __repr__(self):
+
+        return 'ApiClient(endpoint=%r)' % self.endpoint
+
+    def get_alerts(self, **kwargs):
+
+        return self._get('/api/alerts', kwargs)
+
+    def get_history(self, **kwargs):
+
+        return self._get('/api/alerts/history', kwargs)
+
+    def send_alert(self, alert):
+
+        return self._post('/api/alert', data=str(alert))
 
     def send(self, msg):
 
-        LOG.debug('header = %s', msg.get_header())
-        LOG.debug('message = %s', msg.get_body())
-
-        if msg.event_type.endswith('Alert'):
-            url = 'http://%s:%s%s/alert' % (self.host, self.port, self.root)
-        elif msg.event_type == 'Heartbeat':
-            url = 'http://%s:%s%s/heartbeat' % (self.host, self.port, self.root)
+        if msg.event_type == 'Heartbeat':
+            return self.send_heartbeat(msg)
         else:
-            LOG.error('Message type %s not supported by this API endpoint.', msg.event_type)
+            return self.send_alert(msg)
+
+    def get_alert(self, alertid):
+
+        return self._get('/api/alert/%s' % alertid)
+
+    def tag_alert(self, alertid, tags):
+
+        if not isinstance(tags, list):
             raise
 
-        payload = json.dumps(msg.get_body(), ensure_ascii=False, cls=DateEncoder)
+        return self._post('/api/alert/%s/tag' % alertid, data=json.dumps({"tags": tags}))
+
+    def open_alert(self, alertid):
+
+        self.update_status(alertid, 'open')
+
+    def ack_alert(self, alertid):
+
+        self.update_status(alertid, 'ack')
+
+    def unack_alert(self, alertid):
+
+        self.open_alert(alertid)
+
+    def assign_alert(self, alertid):
+
+        self.update_status(alertid, 'assigned')
+
+    def close_alert(self, alertid):
+
+        self.update_status(alertid, 'closed')
+
+    def update_status(self, alertid, status):
+
+        return self._post('/api/alert/%s/status' % alertid, data=json.dumps({"status": status}))
+
+    def delete_alert(self, alertid):
+
+        return self._delete('/api/alert/%s' % alertid)
+
+    def send_heartbeat(self, heartbeat):
+        """
+        Send a heartbeat
+        """
+        return self._post('/api/heartbeat', data=str(heartbeat))
+
+    def get_heartbeats(self):
+        """
+        Get list of heartbeats
+        """
+        return self._get('/api/heartbeats')
+
+    def delete_heartbeat(self, heartbeatid):
+
+        return self._delete('/api/heartbeat/%s' % heartbeatid)
+
+    def _get(self, path, query=None):
+
+        url = self.endpoint + path + '?' + urllib.urlencode(query)
+        response = requests.get(url)
+
+        LOG.debug('Content type from response: %s', response.headers['content-type'])
+        LOG.debug('Response Headers: %s', response.headers)
+        LOG.debug('Response Body: %s', response.text)
+
+        try:
+            response.raise_for_status()
+        except requests.HTTPError:
+            raise
+
+        return response.json()
+
+    def _post(self, path, data=None):
+
+        url = self.endpoint + path
         headers = {'Content-Type': 'application/json'}
 
-        if CONF.dry_run:
-            print "curl -v '%s' -H 'Content-Type: application/json' -d '%s'" % (url, payload)
-            sys.exit(0)
+        LOG.debug('Request Headers: %s', headers)
+        LOG.debug('Request Body: %s', data)
 
-        LOG.debug('Sending alert to API endpoint...')
-        try:
-            r = requests.post(url, data=payload, headers=headers)
-        except requests.Timeout, e:
-            LOG.warning('API request timed out: %s', e)
-            raise
-        except requests.ConnectionError, e:
-            LOG.error('API request connection error: %s', e)
-            raise
-        except requests.TooManyRedirects, e:
-            LOG.error('Too many redirects: %s', e)
-            raise
-        except requests.RequestException, e:
-            LOG.error('API request send failed: %s', e)
-            raise
-        except requests.ConnectionError, e:
-            LOG.error('API request send failed: %s', e)
-            raise
-
-        LOG.debug('HTTP status: %s', r.status_code)
-        LOG.debug('HTTP response: %s', r.text)
+        response = requests.post(url, data=data, headers=headers)
 
         try:
-            r.raise_for_status()
-        except requests.HTTPError, e:
-            LOG.error('API request HTTP error: %s', e)
+            response.raise_for_status()
+        except requests.HTTPError:
             raise
+
+        return response.json()
+
+    def _delete(self, path):
+
+        url = self.endpoint + path
+        response = requests.delete(url)
 
         try:
-            response = r.json()
-        except Exception, e:
-            LOG.error('API bad response - %s: %s', e, r.text)
+            response.raise_for_status()
+        except requests.HTTPError:
             raise
 
-        if response.get('status', None) == 'ok':
-            LOG.debug('API request successful: %s', response)
-            return response['id']
-        elif response.get('status', None) == 'error' and 'message' in response:
-            LOG.error('API request failed: %s', response['message'])
-            raise
-        else:
-            LOG.error('API request unknown error: %s', response)
-            raise
-
-    def query(self, query=None):
-
-        LOG.debug('query = %s', query)
-
-        url = 'http://%s:%s%s/alerts' % (self.host, self.port, self.root)
-
-        if query:
-            url = "%s?%s" % (url, urllib.urlencode(query, doseq=1))
-
-        if CONF.dry_run:
-            print "curl -v '%s' -H 'Content-Type: application/json'" % url
-            sys.exit(0)
-
-        LOG.debug('Querying API endpoint...')
-        try:
-            r = requests.get(url)
-        except requests.Timeout, e:
-            LOG.warning('API request timed out: %s', e)
-            raise
-        except requests.ConnectionError, e:
-            LOG.error('API request connection error: %s', e)
-            raise
-        except requests.TooManyRedirects, e:
-            LOG.error('Too many redirects: %s', e)
-            raise
-        except requests.RequestException, e:
-            LOG.error('API request send failed: %s', e)
-            raise
-
-        LOG.debug('HTTP status: %s', r.status_code)
-        LOG.debug('HTTP response: %s', r.text)
-
-        try:
-            r.raise_for_status()
-        except requests.HTTPError, e:
-            LOG.error('API request HTTP error: %s', e)
-            raise
-
-        try:
-            response = r.json()
-        except Exception, e:
-            LOG.error('API bad response - %s: %s', e, r.text)
-            raise
-
-        if response.get('status', None) == 'ok':
-            LOG.debug('API request successful: %s', response)
-            return response
-        elif response.get('status', None) == 'error' and 'message' in response:
-            LOG.error('API request failed: %s', response['message'])
-            raise
-        else:
-            LOG.error('API request unknown error: %s', response)
-            raise
-
-    def ack(self, alertid):
-
-        LOG.debug('alertid = %s', alertid)
-
-        url = 'http://%s:%s%s/alert/%s/status' % (self.host, self.port, self.root, alertid)
-
-        data = {"status": "ack", "text": "ack via API"}
-
-        payload = json.dumps({"status": "ack", "text": "ack via API"})
-        headers = {'Content-Type': 'application/json'}
-
-        if CONF.dry_run:
-            print "curl -v -X PUT '%s' -H 'Content-Type: application/json' -d '%s'" % (url, data)
-            sys.exit(0)
-
-        LOG.debug('Acking via API endpoint...')
-        try:
-            r = requests.post(url, data=payload, headers=headers)
-        except requests.Timeout, e:
-            LOG.warning('API request timed out: %s', e)
-            raise
-        except requests.ConnectionError, e:
-            LOG.error('API request connection error: %s', e)
-            raise
-        except requests.TooManyRedirects, e:
-            LOG.error('Too many redirects: %s', e)
-            raise
-        except requests.RequestException, e:
-            LOG.error('API request send failed: %s', e)
-            raise
-
-        LOG.debug('HTTP status: %s', r.status_code)
-        LOG.debug('HTTP response: %s', r.text)
-
-        try:
-            r.raise_for_status()
-        except requests.HTTPError, e:
-            LOG.error('API request HTTP error: %s', e)
-            raise
-
-        try:
-            response = r.json()
-        except Exception, e:
-            LOG.error('API bad response - %s: %s', e, r.text)
-            raise
-
-        if response.get('status', None) == 'ok':
-            LOG.debug('API request successful: %s', response)
-            return response
-        elif response.get('status', None) == 'error' and 'message' in response:
-            LOG.error('API request failed: %s', response['message'])
-            raise
-        else:
-            LOG.error('API request unknown error: %s', response)
-            raise
-
-    def delete(self, alertid):
-
-        LOG.debug('alertid = %s', alertid)
-
-        url = 'http://%s:%s%s/alert/%s' % (self.host, self.port, self.root, alertid)
-
-        if CONF.dry_run:
-            print "curl -v -X DELETE '%s' -H 'Content-Type: application/json'" % url
-            sys.exit(0)
-
-        LOG.debug('Deleting via API endpoint...')
-        try:
-            r = requests.delete(url)
-        except requests.Timeout, e:
-            LOG.warning('API request timed out: %s', e)
-            raise
-        except requests.ConnectionError, e:
-            LOG.error('API request connection error: %s', e)
-            raise
-        except requests.TooManyRedirects, e:
-            LOG.error('Too many redirects: %s', e)
-            raise
-        except requests.RequestException, e:
-            LOG.error('API request send failed: %s', e)
-            raise
-
-        LOG.debug('HTTP status: %s', r.status_code)
-        LOG.debug('HTTP response: %s', r.text)
-
-        try:
-            r.raise_for_status()
-        except requests.HTTPError, e:
-            LOG.error('API request HTTP error: %s', e)
-            raise
-
-        try:
-            response = r.json()
-        except Exception, e:
-            LOG.error('API bad response - %s: %s', e, r.text)
-            raise
-
-        if response.get('status', None) == 'ok':
-            LOG.debug('API request successful: %s', response)
-            return response
-        elif response.get('status', None) == 'error' and 'message' in response:
-            LOG.error('API request failed: %s', response['message'])
-            raise
-        else:
-            LOG.error('API request unknown error: %s', response)
-            raise
+        return response.json()
