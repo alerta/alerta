40a508b84b022e8e1e70d20f4f65753b70074eac
Nick Satterly <nick.satterly@guardian.co.uk>
2014-07-26 22:29:26 +0100

    fix logstash plug-in

--- alerta/plugins/logstash.py
+++ alerta/plugins/logstash.py
@@ -12,15 +12,6 @@ class LogStashOutput(PluginBase):
 
     def send(self, alert):
 
-        data = {
-            "@version": "1",
-            "@timestamp": alert.last_receive_time,
-            "message": alert.text,
-            "source_host": alert.resource
-        }
-
-        data.update(dict(alert.get_body()))
-
         try:
             self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.sock.connect((settings.LOGSTASH_HOST, settings.LOGSTASH_PORT))
@@ -28,7 +19,7 @@ class LogStashOutput(PluginBase):
             raise RuntimeError("Logstash connection error")
 
         try:
-            self.sock.send("%s\r\n" % data)
+            self.sock.send("%s\r\n" % alert)
         except Exception as e:
             LOG.exception(e)
             raise RuntimeError("logstash exception")
