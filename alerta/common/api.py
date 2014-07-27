37ca7ba528b9b719606670b37fb47c5cebb3c40c
Nick Satterly <nick.satterly@guardian.co.uk>
2014-07-27 19:27:03 +0100

    sync api module with client

--- alerta/common/api.py
+++ alerta/common/api.py
@@ -4,24 +4,43 @@ import urllib
 import requests
 import logging
 
-__version__ = '3.2.0'
+from requests.auth import AuthBase
 
 LOG = logging.getLogger(__name__)
+
+DEFAULT_TIMEOUT = 86400
+
+
+class ApiAuth(AuthBase):
+
+    def __init__(self, key):
+
+        self.key = key
+
+    def __call__(self, r):
+
+        r.headers['Authorization'] = 'Key %s' % self.key
+        return r
 
 
 class ApiClient(object):
 
-    def __init__(self, endpoint='http:/localhost:8080'):
+    def __init__(self, endpoint="http://localhost:8080", key=''):
 
         self.endpoint = endpoint
+        self.key = key
 
     def __repr__(self):
 
-        return 'ApiClient(endpoint=%r)' % self.endpoint
+        return 'ApiClient(endpoint=%r, key=%r)' % (self.endpoint, self.key)
 
     def get_alerts(self, **kwargs):
 
         return self._get('/api/alerts', kwargs)
+
+    def get_counts(self, **kwargs):
+
+        return self._get('/api/alerts/count', kwargs)
 
     def get_history(self, **kwargs):
 
@@ -48,6 +67,13 @@ class ApiClient(object):
             raise
 
         return self._post('/api/alert/%s/tag' % alertid, data=json.dumps({"tags": tags}))
+
+    def untag_alert(self, alertid, tags):
+
+        if not isinstance(tags, list):
+            raise
+
+        return self._post('/api/alert/%s/untag' % alertid, data=json.dumps({"tags": tags}))
 
     def open_alert(self, alertid):
 
@@ -93,10 +119,10 @@ class ApiClient(object):
 
         return self._delete('/api/heartbeat/%s' % heartbeatid)
 
-    def _get(self, path, query=None):
+    def _get(self, path, query=''):
 
         url = self.endpoint + path + '?' + urllib.urlencode(query, doseq=True)
-        response = requests.get(url)
+        response = requests.get(url, auth=ApiAuth(self.key))
 
         LOG.debug('Content type from response: %s', response.headers['content-type'])
         LOG.debug('Response Headers: %s', response.headers)
@@ -117,7 +143,7 @@ class ApiClient(object):
         LOG.debug('Request Headers: %s', headers)
         LOG.debug('Request Body: %s', data)
 
-        response = requests.post(url, data=data, headers=headers)
+        response = requests.post(url, data=data, auth=ApiAuth(self.key), headers=headers)
 
         try:
             response.raise_for_status()
@@ -129,7 +155,7 @@ class ApiClient(object):
     def _delete(self, path):
 
         url = self.endpoint + path
-        response = requests.delete(url)
+        response = requests.delete(url, auth=ApiAuth(self.key))
 
         try:
             response.raise_for_status()
