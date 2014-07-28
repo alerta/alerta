bae68b1eb8f725f1a485ea077492a8d594783c38
Nick Satterly <nick.satterly@guardian.co.uk>
2014-07-28 13:35:36 +0100

    minor changes to logging

--- alerta/app/__init__.py
+++ alerta/app/__init__.py
@@ -9,7 +9,7 @@ app.config.from_object('alerta.default_settings')
 app.config.from_object('alerta.settings')
 
 if app.config['LOG_FILE']:
-    file_handler = RotatingFileHandler(filename=app.config['LOG_FILE'], maxBytes=10000, backupCount=1)
+    file_handler = RotatingFileHandler(filename=app.config['LOG_FILE'], encoding='utf-8', maxBytes=10000, backupCount=1)
     file_handler.setFormatter(logging.Formatter(fmt=app.config['LOG_FORMAT']))
     app.logger.addHandler(file_handler)
 
@@ -20,6 +20,8 @@ if app.config['USE_SYSLOG']:
 
 if app.debug:
     app.logger.setLevel(logging.DEBUG)
+else:
+    app.logger.setLevel(logging.WARNING)
 
 from alerta.app.database import Mongo
 
