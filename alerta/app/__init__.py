886b5356ad744679faf7304e708cfc1b93fbfc97
Nick Satterly <nick.satterly@guardian.co.uk>
2014-08-14 20:03:31 +0100

    add a default config file of /etc/alertad.conf

--- alerta/app/__init__.py
+++ alerta/app/__init__.py
@@ -7,6 +7,7 @@ from flask import Flask
 
 app = Flask(__name__)
 app.config.from_object('alerta.settings')
+app.config.from_pyfile('/etc/alertad.conf', silent=True)
 app.config.from_envvar('ALERTA_CONF_FILE', silent=True)
 
 if app.config['USE_STDERR']:
