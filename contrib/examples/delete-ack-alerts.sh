6506f046008868e0802affe05b66f1bde7c05484
Nick Satterly <nick.satterly@guardian.co.uk>
2014-12-15 16:14:50 +0000

    more spring cleaning

--- contrib/examples/delete-ack-alerts.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Script to delete all ACK'ed alerts
-
-/opt/alerta/sbin/alert-query.py --status ACK --no-header --no-footer | awk -F"|" ' { print $1 } ' | while read id
-do
-    curl -XDELETE http://${1:-'monitoring.gudev.gnl'}/alerta/api/v1/alerts/alert/$id
-done
--- contrib/examples/snmptrap.sh
+++ contrib/examples/snmptrap.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
-set -x
+if [ "$1" == "-h" ]
+then
+  echo "Usage: snmptrap.sh [TRAP_DEST] [COMMUNITY]"
+  exit 1
+fi
 
-snmptrap -v2c -c ${2:-'public'} ${1:-'monitoring.gudev.gnl'} "" .1.3.6.1.6.3.1.1.5.1.0 0 s "This is a test coldStart trap"
-snmptrap -v2c -c ${2:-'public'} ${1:-'monitoring.gudev.gnl'} "" .1.3.6.1.6.3.1.1.5.2.0 0 s "This is a test warmStart trap"
-snmptrap -v2c -c ${2:-'public'} ${1:-'monitoring.gudev.gnl'} "" .1.3.6.1.6.3.1.1.5.3.0 0 s "This is a test linkDown trap"
-snmptrap -v2c -c ${2:-'public'} ${1:-'monitoring.gudev.gnl'} "" .1.3.6.1.6.3.1.1.5.4.0 0 s "This is a test linkUp trap"
-snmptrap -v2c -c ${2:-'public'} ${1:-'monitoring.gudev.gnl'} "" .1.3.6.1.6.3.1.1.5.5.0 0 s "This is a test authenticationFailure trap"
+TRAP_DEST=${1:-localhost}
+COMMUNITY=${2:-public}
+
+snmptrap -v2c -c ${COMMUNITY} ${TRAP_DEST} "" .1.3.6.1.6.3.1.1.5.1.0 0 s "This is a test coldStart trap"
+snmptrap -v2c -c ${COMMUNITY} ${TRAP_DEST} "" .1.3.6.1.6.3.1.1.5.2.0 0 s "This is a test warmStart trap"
+snmptrap -v2c -c ${COMMUNITY} ${TRAP_DEST} "" .1.3.6.1.6.3.1.1.5.3.0 0 s "This is a test linkDown trap"
+snmptrap -v2c -c ${COMMUNITY} ${TRAP_DEST} "" .1.3.6.1.6.3.1.1.5.4.0 0 s "This is a test linkUp trap"
+snmptrap -v2c -c ${COMMUNITY} ${TRAP_DEST} "" .1.3.6.1.6.3.1.1.5.5.0 0 s "This is a test authenticationFailure trap"
 
