0f816803256ab77febfbad4883fadf9f10f5ac36
Nick Satterly <nick.satterly@guardian.co.uk>
2015-02-06 14:23:23 +0000

    add script to create demo key

---
+++ scripts/mk_demo_key.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import datetime
+
+from alerta.app.database import Mongo
+
+data = {
+    "user": "test user",
+    "key": "demo-key",
+    "text": "demo key",
+    "expireTime": datetime.datetime.utcnow() + datetime.timedelta(365),
+    "count": 0,
+    "lastUsedTime": None
+}
+
+Mongo().db.keys.insert(data)
+
