1fae3b0c5ab0652306ca487ab23181a38dff9a23
Nick Satterly <nick.satterly@guardian.co.uk>
2015-06-11 14:26:21 +0100

    add api key tests

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,7 +1,7 @@
 import json
 import unittest
 
-from alerta.app import app
+from alerta.app import app, db
 
 
 class AppTestCase(unittest.TestCase):
@@ -19,24 +19,61 @@ class AppTestCase(unittest.TestCase):
             'service': ['Quux']
         }
 
+        self.api_key = db.create_key('demo-key', type='read-write', text='demo-key')
+
     def tearDown(self):
 
         pass
 
-    def test_new_alert(self):
+    def test_401_error(self):
 
-        response = self.app.post('/alert', data=json.dumps(self.alert), headers={'Content-type': 'application/json'})
+        response = self.app.get('/alerts')
+        self.assertEqual(response.status_code, 401)
+
+    def test_readwrite_key(self):
+
+        headers = {
+            'Authorization': 'Key %s' % self.api_key
+        }
+
+        response = self.app.post('/alert', data=json.dumps(self.alert), headers=headers)
         self.assertEqual(response.status_code, 201)
         data = json.loads(response.data)
         self.assertListEqual(self.alert['service'], data['alert']['service'])
 
         new_alert_id = data['id']
 
-        response = self.app.get('/alert/' + new_alert_id)
+        response = self.app.get('/alert/' + new_alert_id, headers=headers)
         self.assertEqual(response.status_code, 200)
         data = json.loads(response.data)
         self.assertIn(new_alert_id, data['alert']['id'])
 
-    def test_api_keys(self):
+    def test_readonly_key(self):
 
-        pass
+        headers = {
+            'Authorization': 'Key %s' % self.api_key,
+            'Content-type': 'application/json'
+        }
+        payload = {
+            'user': 'ro-demo-key',
+            'type': 'read-only'
+        }
+
+        response = self.app.post('/key', data=json.dumps(payload), headers=headers)
+        self.assertEqual(response.status_code, 201)
+        data = json.loads(response.data)
+        self.assertIsNotNone(data['key'], 'Failed to create read-only key')
+
+        read_only_key = data['key']
+
+        response = self.app.post('/alert', data=json.dumps(self.alert), headers={'Authorization': 'Key ' + read_only_key})
+        self.assertEqual(response.status_code, 403)
+
+        response = self.app.get('/alerts', headers={'Authorization': 'Key ' + read_only_key})
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.data)
+        self.assertGreater(data['total'], 1, "total alerts > 1")
+
+        response = self.app.delete('/key/' + read_only_key, headers={'Authorization': 'Key ' + self.api_key})
+        self.assertEqual(response.status_code, 200)
+
