abc2491a50ad56eeebe90c8d1d14dd6a41c945a5
Nick Satterly <nick.satterly@guardian.co.uk>
2015-07-30 12:49:38 +0100

    Decode flask response

--- tests/test_alerts.py
+++ tests/test_alerts.py
@@ -76,7 +76,7 @@ class AlertTestCase(unittest.TestCase):
         # create alert
         response = self.app.post('/alert', data=json.dumps(self.major_alert), headers=self.headers)
         self.assertEqual(response.status_code, 201)
-        data = json.loads(response.data, encoding='utf-8')
+        data = json.loads(response.data.decode('utf-8'))
         self.assertEqual(data['alert']['resource'], self.resource)
         self.assertEqual(data['alert']['status'], 'open')
         self.assertEqual(data['alert']['duplicateCount'], 0)
@@ -87,7 +87,7 @@ class AlertTestCase(unittest.TestCase):
         # create duplicate alert
         response = self.app.post('/alert', data=json.dumps(self.major_alert), headers=self.headers)
         self.assertEqual(response.status_code, 201)
-        data = json.loads(response.data, encoding='utf-8')
+        data = json.loads(response.data.decode('utf-8'))
         self.assertIn(alert_id, data['alert']['id'])
         self.assertEqual(data['alert']['duplicateCount'], 1)
         self.assertEqual(data['alert']['previousSeverity'], 'unknown')
@@ -96,7 +96,7 @@ class AlertTestCase(unittest.TestCase):
         # correlate alert (same event, diff sev)
         response = self.app.post('/alert', data=json.dumps(self.critical_alert), headers=self.headers)
         self.assertEqual(response.status_code, 201)
-        data = json.loads(response.data, encoding='utf-8')
+        data = json.loads(response.data.decode('utf-8'))
         self.assertIn(alert_id, data['alert']['id'])
         self.assertEqual(data['alert']['status'], 'open')
         self.assertEqual(data['alert']['duplicateCount'], 0)
@@ -107,7 +107,7 @@ class AlertTestCase(unittest.TestCase):
         # de-duplicate
         response = self.app.post('/alert', data=json.dumps(self.critical_alert), headers=self.headers)
         self.assertEqual(response.status_code, 201)
-        data = json.loads(response.data, encoding='utf-8')
+        data = json.loads(response.data.decode('utf-8'))
         self.assertIn(alert_id, data['alert']['id'])
         self.assertEqual(data['alert']['duplicateCount'], 1)
         self.assertEqual(data['alert']['trendIndication'], 'moreSevere')
@@ -115,7 +115,7 @@ class AlertTestCase(unittest.TestCase):
         # correlate alert (diff event, same sev)
         response = self.app.post('/alert', data=json.dumps(self.fatal_alert), headers=self.headers)
         self.assertEqual(response.status_code, 201)
-        data = json.loads(response.data, encoding='utf-8')
+        data = json.loads(response.data.decode('utf-8'))
         self.assertIn(alert_id, data['alert']['id'])
         self.assertEqual(data['alert']['duplicateCount'], 0)
         self.assertEqual(data['alert']['previousSeverity'], self.critical_alert['severity'])
@@ -125,7 +125,7 @@ class AlertTestCase(unittest.TestCase):
         # correlate alert (diff event, diff sev)
         response = self.app.post('/alert', data=json.dumps(self.major_alert), headers=self.headers)
         self.assertEqual(response.status_code, 201)
-        data = json.loads(response.data, encoding='utf-8')
+        data = json.loads(response.data.decode('utf-8'))
         self.assertIn(alert_id, data['alert']['id'])
         self.assertEqual(data['alert']['duplicateCount'], 0)
         self.assertEqual(data['alert']['previousSeverity'], self.fatal_alert['severity'])
@@ -134,7 +134,7 @@ class AlertTestCase(unittest.TestCase):
         # correlate alert (diff event, diff sev)
         response = self.app.post('/alert', data=json.dumps(self.normal_alert), headers=self.headers)
         self.assertEqual(response.status_code, 201)
-        data = json.loads(response.data, encoding='utf-8')
+        data = json.loads(response.data.decode('utf-8'))
         self.assertIn(alert_id, data['alert']['id'])
         self.assertEqual(data['alert']['status'], 'closed')
         self.assertEqual(data['alert']['duplicateCount'], 0)
@@ -144,7 +144,7 @@ class AlertTestCase(unittest.TestCase):
         # get alert
         response = self.app.get('/alert/' + alert_id)
         self.assertEqual(response.status_code, 200)
-        data = json.loads(response.data, encoding='utf-8')
+        data = json.loads(response.data.decode('utf-8'))
         self.assertIn(alert_id, data['alert']['id'])
 
         # delete alert
@@ -161,13 +161,13 @@ class AlertTestCase(unittest.TestCase):
         # create alert
         response = self.app.post('/alert', data=json.dumps(self.normal_alert), headers=self.headers)
         self.assertEqual(response.status_code, 201)
-        data = json.loads(response.data, encoding='utf-8')
+        data = json.loads(response.data.decode('utf-8'))
 
         alert_id = data['id']
 
         response = self.app.get('/alerts')
         self.assertEqual(response.status_code, 200)
-        data = json.loads(response.data, encoding='utf-8')
+        data = json.loads(response.data.decode('utf-8'))
         self.assertGreater(data['total'], 0)
 
         # delete alert
@@ -179,7 +179,7 @@ class AlertTestCase(unittest.TestCase):
         # create alert (status=open)
         response = self.app.post('/alert', data=json.dumps(self.warn_alert), headers=self.headers)
         self.assertEqual(response.status_code, 201)
-        data = json.loads(response.data, encoding='utf-8')
+        data = json.loads(response.data.decode('utf-8'))
         self.assertEqual(data['alert']['status'], 'open')
 
         alert_id = data['id']
@@ -187,7 +187,7 @@ class AlertTestCase(unittest.TestCase):
         # severity != normal -> status=open
         response = self.app.post('/alert', data=json.dumps(self.major_alert), headers=self.headers)
         self.assertEqual(response.status_code, 201)
-        data = json.loads(response.data, encoding='utf-8')
+        data = json.loads(response.data.decode('utf-8'))
         self.assertEqual(data['alert']['status'], 'open')
 
         # ack alert
@@ -195,13 +195,13 @@ class AlertTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         response = self.app.get('/alert/' + alert_id)
         self.assertEqual(response.status_code, 200)
-        data = json.loads(response.data, encoding='utf-8')
+        data = json.loads(response.data.decode('utf-8'))
         self.assertEqual(data['alert']['status'], 'ack')
 
         # new severity > old severity -> status=open
         response = self.app.post('/alert', data=json.dumps(self.critical_alert), headers=self.headers)
         self.assertEqual(response.status_code, 201)
-        data = json.loads(response.data, encoding='utf-8')
+        data = json.loads(response.data.decode('utf-8'))
         self.assertEqual(data['alert']['status'], 'open')
 
         # ack alert (again)
@@ -209,31 +209,31 @@ class AlertTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         response = self.app.get('/alert/' + alert_id)
         self.assertEqual(response.status_code, 200)
-        data = json.loads(response.data, encoding='utf-8')
+        data = json.loads(response.data.decode('utf-8'))
         self.assertEqual(data['alert']['status'], 'ack')
 
         # new severity <= old severity -> status=ack
         response = self.app.post('/alert', data=json.dumps(self.major_alert), headers=self.headers)
         self.assertEqual(response.status_code, 201)
-        data = json.loads(response.data, encoding='utf-8')
+        data = json.loads(response.data.decode('utf-8'))
         self.assertEqual(data['alert']['status'], 'ack')
 
         # severity == normal -> status=closed
         response = self.app.post('/alert', data=json.dumps(self.normal_alert), headers=self.headers)
         self.assertEqual(response.status_code, 201)
-        data = json.loads(response.data, encoding='utf-8')
+        data = json.loads(response.data.decode('utf-8'))
         self.assertEqual(data['alert']['status'], 'closed')
 
         # severity != normal -> status=open
         response = self.app.post('/alert', data=json.dumps(self.warn_alert), headers=self.headers)
         self.assertEqual(response.status_code, 201)
-        data = json.loads(response.data, encoding='utf-8')
+        data = json.loads(response.data.decode('utf-8'))
         self.assertEqual(data['alert']['status'], 'open')
 
         # severity = normal -> status=closed
         response = self.app.post('/alert', data=json.dumps(self.normal_alert), headers=self.headers)
         self.assertEqual(response.status_code, 201)
-        data = json.loads(response.data, encoding='utf-8')
+        data = json.loads(response.data.decode('utf-8'))
         self.assertEqual(data['alert']['status'], 'closed')
 
         # delete alert
@@ -245,7 +245,7 @@ class AlertTestCase(unittest.TestCase):
         # create alert
         response = self.app.post('/alert', data=json.dumps(self.fatal_alert), headers=self.headers)
         self.assertEqual(response.status_code, 201)
-        data = json.loads(response.data, encoding='utf-8')
+        data = json.loads(response.data.decode('utf-8'))
         self.assertEqual(data['alert']['tags'], ['foo'])
 
         alert_id = data['id']
@@ -255,7 +255,7 @@ class AlertTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         response = self.app.get('/alert/' + alert_id)
         self.assertEqual(response.status_code, 200)
-        data = json.loads(response.data, encoding='utf-8')
+        data = json.loads(response.data.decode('utf-8'))
         self.assertEqual(data['alert']['tags'], ['foo', 'bar'])
 
         # duplicate tag is a no-op
@@ -263,7 +263,7 @@ class AlertTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         response = self.app.get('/alert/' + alert_id)
         self.assertEqual(response.status_code, 200)
-        data = json.loads(response.data, encoding='utf-8')
+        data = json.loads(response.data.decode('utf-8'))
         self.assertEqual(data['alert']['tags'], ['foo', 'bar'])
 
         # delete tag
@@ -271,7 +271,7 @@ class AlertTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         response = self.app.get('/alert/' + alert_id)
         self.assertEqual(response.status_code, 200)
-        data = json.loads(response.data, encoding='utf-8')
+        data = json.loads(response.data.decode('utf-8'))
         self.assertEqual(data['alert']['tags'], ['bar'])
 
     def test_aggregations(self):
@@ -279,24 +279,24 @@ class AlertTestCase(unittest.TestCase):
         # counts
         response = self.app.get('/alerts/count')
         self.assertEqual(response.status_code, 200)
-        data = json.loads(response.data, encoding='utf-8')
+        data = json.loads(response.data.decode('utf-8'))
         self.assertIn('statusCounts', data)
         self.assertIn('severityCounts', data)
 
         # top10
         response = self.app.get('/alerts/top10')
         self.assertEqual(response.status_code, 200)
-        data = json.loads(response.data, encoding='utf-8')
+        data = json.loads(response.data.decode('utf-8'))
         self.assertIn('top10', data)
 
         # environments
         response = self.app.get('/environments')
         self.assertEqual(response.status_code, 200)
-        data = json.loads(response.data, encoding='utf-8')
+        data = json.loads(response.data.decode('utf-8'))
         self.assertIn('environments', data)
 
         # service
         response = self.app.get('/services')
         self.assertEqual(response.status_code, 200)
-        data = json.loads(response.data, encoding='utf-8')
+        data = json.loads(response.data.decode('utf-8'))
         self.assertIn('services', data)
