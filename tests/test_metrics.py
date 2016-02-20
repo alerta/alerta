dc1ba1af30e960886c4c8a2e92d4c40963f1af07
Nick Satterly <nick.satterly@guardian.co.uk>
2016-02-20 16:21:20 +0000

    Fix tests

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -16,14 +16,14 @@ class MetricsTestCase(unittest.TestCase):
         test_gauge = Gauge(group='test', name='gauge', title='Test gauge', description='total time to process timed events')
         test_gauge.set(500)
 
-        gauge = [t for t in Gauge.get_gauges() if t['title'] == 'Test gauge'][0]
-        self.assertGreaterEqual(gauge['value'], 500)
+        gauge = [g for g in Gauge.get_gauges() if g.title == 'Test gauge'][0]
+        self.assertGreaterEqual(gauge.value, 500)
 
         test_timer = Timer(group='test', name='timer', title='Test timer', description='total time to process timed events')
         recv_started = test_timer.start_timer()
         time.sleep(1)
         test_timer.stop_timer(recv_started)
 
-        timer = [t for t in Timer.get_timers() if t['title'] == 'Test timer'][0]
-        self.assertGreaterEqual(timer['count'], 1)
-        self.assertGreaterEqual(timer['totalTime'], 999)
+        timer = [t for t in Timer.get_timers() if t.title == 'Test timer'][0]
+        self.assertGreaterEqual(timer.count, 1)
+        self.assertGreaterEqual(timer.total_time, 999)
