47d26198aa9ea52f4671b553213d733b51d974fd
Nick Satterly <nick.satterly@theguardian.com>
2017-07-19 22:53:21 +0100

    Move SAML auth to documented order

--- alerta/app/auth.py
+++ alerta/app/auth.py
@@ -16,6 +16,7 @@ from flask_cors import cross_origin
 from jwt import DecodeError, ExpiredSignature, InvalidAudience
 from base64 import urlsafe_b64decode
 from uuid import uuid4
+
 try:
     import saml2
     import saml2.entity
@@ -318,98 +319,6 @@ def verify_email(hash):
         return render_template('auth/verify_failed.html')
 
 
-if 'SAML2_CONFIG' in app.config:
-    spConfig = saml2.config.Config()
-    saml2_config_default = {
-        'entityid': absolute_url(),
-        'service': {
-            'sp': {
-                'endpoints': {
-                    'assertion_consumer_service': [
-                        (absolute_url('/auth/saml'), saml2.BINDING_HTTP_POST)
-                    ]
-                }
-            }
-        }
-    }
-    spConfig.load(deepmerge(saml2_config_default, app.config['SAML2_CONFIG']))
-    saml_client = saml2.client.Saml2Client(config=spConfig)
-
-
-@app.route('/auth/saml', methods=['GET'])
-def saml_redirect_to_idp():
-    relay_state = None if request.args.get('usePostMessage') is None else 'usePostMessage'
-    (session_id, result) = saml_client.prepare_for_authenticate(relay_state=relay_state)
-    return make_response('', 302, result['headers'])
-
-
-@app.route('/auth/saml', methods=['OPTIONS', 'POST'])
-@cross_origin(supports_credentials=True)
-def saml_response_from_idp():
-    def _make_response(resp_obj, resp_code):
-        if 'usePostMessage' in request.form.get('RelayState', '') and 'text/html' in request.headers.get('Accept', ''):
-            origins = app.config.get('CORS_ORIGINS', [])
-            response = make_response(
-                '''<!DOCTYPE html>
-                    <html lang="en">
-                        <head>
-                            <meta charset="UTF-8">
-                            <title>Authenticating...</title>
-                            <script type="application/javascript">
-                                var origins = {origins};
-                                // in case when API and WebUI are on the same origin
-                                if (origins.indexOf(window.location.origin) < 0)
-                                    origins.push(window.location.origin);
-                                // only one will succeed
-                                origins.forEach(origin => window.opener.postMessage({msg_data}, origin));
-                                window.close();
-                            </script>
-                        </head>
-                        <body></body>
-                    </html>'''.format(msg_data=json.dumps(resp_obj), origins=json.dumps(origins)),
-                resp_code
-            )
-            response.headers['Content-Type'] = 'text/html'
-            return response
-        else:
-            return jsonify(**resp_obj), resp_code
-
-    authn_response = saml_client.parse_authn_request_response(
-        request.form['SAMLResponse'],
-        saml2.entity.BINDING_HTTP_POST
-    )
-    identity = authn_response.get_identity()
-    email = identity['emailAddress'][0]
-    name = (app.config.get('SAML2_USER_NAME_FORMAT', '{givenName} {surname}')).format(**dict(map(lambda x: (x[0], x[1][0]), identity.items())))
-
-    groups = identity.get('groups', [])
-    if app.config['AUTH_REQUIRED'] and not ('*' in app.config['ALLOWED_SAML2_GROUPS']
-            or set(app.config['ALLOWED_SAML2_GROUPS']).intersection(set(groups))):
-        return _make_response({'status': 'error', 'message': 'User {} is not authorized'.format(email)}, 403)
-
-    if app.config['CUSTOMER_VIEWS']:
-        try:
-            customer = customer_match(email, groups=[email.split('@')[1]])
-        except NoCustomerMatch:
-            return _make_response(
-                {'status': 'error', 'message': 'No customer lookup defined for user %s' % email},
-                403
-            )
-    else:
-        customer = None
-
-    token = create_token(email, name, email, provider='saml2', customer=customer, scopes=scopes(email, groups=groups))
-    return _make_response({'status': 'ok', 'token': token}, 200)
-
-
-@app.route('/auth/saml/metadata.xml', methods=['GET'])
-def saml_metadata():
-    edesc = saml2.metadata.entity_descriptor(spConfig)
-    response = make_response(str(edesc))
-    response.headers['Content-Type'] = 'text/xml; charset=utf-8'
-    return response
-
-
 @app.route('/auth/google', methods=['OPTIONS', 'POST'])
 @cross_origin(supports_credentials=True)
 def google():
@@ -604,6 +513,98 @@ def keycloak():
 
     token = create_token(profile['sub'], profile['name'], login, provider='keycloak', customer=customer, scopes=scopes(login, groups=[]))
     return jsonify(token=token)
+
+
+if 'SAML2_CONFIG' in app.config:
+    spConfig = saml2.config.Config()
+    saml2_config_default = {
+        'entityid': absolute_url(),
+        'service': {
+            'sp': {
+                'endpoints': {
+                    'assertion_consumer_service': [
+                        (absolute_url('/auth/saml'), saml2.BINDING_HTTP_POST)
+                    ]
+                }
+            }
+        }
+    }
+    spConfig.load(deepmerge(saml2_config_default, app.config['SAML2_CONFIG']))
+    saml_client = saml2.client.Saml2Client(config=spConfig)
+
+
+@app.route('/auth/saml', methods=['GET'])
+def saml_redirect_to_idp():
+    relay_state = None if request.args.get('usePostMessage') is None else 'usePostMessage'
+    (session_id, result) = saml_client.prepare_for_authenticate(relay_state=relay_state)
+    return make_response('', 302, result['headers'])
+
+
+@app.route('/auth/saml', methods=['OPTIONS', 'POST'])
+@cross_origin(supports_credentials=True)
+def saml_response_from_idp():
+    def _make_response(resp_obj, resp_code):
+        if 'usePostMessage' in request.form.get('RelayState', '') and 'text/html' in request.headers.get('Accept', ''):
+            origins = app.config.get('CORS_ORIGINS', [])
+            response = make_response(
+                '''<!DOCTYPE html>
+                    <html lang="en">
+                        <head>
+                            <meta charset="UTF-8">
+                            <title>Authenticating...</title>
+                            <script type="application/javascript">
+                                var origins = {origins};
+                                // in case when API and WebUI are on the same origin
+                                if (origins.indexOf(window.location.origin) < 0)
+                                    origins.push(window.location.origin);
+                                // only one will succeed
+                                origins.forEach(origin => window.opener.postMessage({msg_data}, origin));
+                                window.close();
+                            </script>
+                        </head>
+                        <body></body>
+                    </html>'''.format(msg_data=json.dumps(resp_obj), origins=json.dumps(origins)),
+                resp_code
+            )
+            response.headers['Content-Type'] = 'text/html'
+            return response
+        else:
+            return jsonify(**resp_obj), resp_code
+
+    authn_response = saml_client.parse_authn_request_response(
+        request.form['SAMLResponse'],
+        saml2.entity.BINDING_HTTP_POST
+    )
+    identity = authn_response.get_identity()
+    email = identity['emailAddress'][0]
+    name = (app.config.get('SAML2_USER_NAME_FORMAT', '{givenName} {surname}')).format(**dict(map(lambda x: (x[0], x[1][0]), identity.items())))
+
+    groups = identity.get('groups', [])
+    if app.config['AUTH_REQUIRED'] and not ('*' in app.config['ALLOWED_SAML2_GROUPS']
+            or set(app.config['ALLOWED_SAML2_GROUPS']).intersection(set(groups))):
+        return _make_response({'status': 'error', 'message': 'User {} is not authorized'.format(email)}, 403)
+
+    if app.config['CUSTOMER_VIEWS']:
+        try:
+            customer = customer_match(email, groups=[email.split('@')[1]])
+        except NoCustomerMatch:
+            return _make_response(
+                {'status': 'error', 'message': 'No customer lookup defined for user %s' % email},
+                403
+            )
+    else:
+        customer = None
+
+    token = create_token(email, name, email, provider='saml2', customer=customer, scopes=scopes(email, groups=groups))
+    return _make_response({'status': 'ok', 'token': token}, 200)
+
+
+@app.route('/auth/saml/metadata.xml', methods=['GET'])
+def saml_metadata():
+    edesc = saml2.metadata.entity_descriptor(spConfig)
+    response = make_response(str(edesc))
+    response.headers['Content-Type'] = 'text/xml; charset=utf-8'
+    return response
 
 
 @app.route('/userinfo', methods=['OPTIONS', 'GET'])
