ea58656979124ab2b066033473f8bc2270de14f2
Nick Satterly <nick.satterly@theguardian.com>
2018-01-26 22:45:47 +0000

    Fix is_flapping function for mongodb

--- alerta/database/backends/mongodb/base.py
+++ alerta/database/backends/mongodb/base.py
@@ -134,11 +134,17 @@ class Backend(Database):
         Return true if alert severity has changed more than X times in Y seconds
         """
         pipeline = [
-            {'$match': {"environment": alert.environment, "resource": alert.resource, "event": alert.event}},
+            {'$match': {
+                "environment": alert.environment,
+                "resource": alert.resource,
+                "event": alert.event,
+                "customer": alert.customer
+            }},
             {'$unwind': '$history'},
-            {'$match': {"history.updateTime": {'$gt': datetime.utcnow() - timedelta(seconds=window)}},
-             "history.type": "severity"
-            },
+            {'$match': {
+                "history.updateTime": {'$gt': datetime.utcnow() - timedelta(seconds=window)},
+                "history.type": "severity"
+            }},
             {'$group': {"_id": '$history.type', "count": {'$sum': 1}}}
         ]
         responses = g.db.alerts.aggregate(pipeline)
