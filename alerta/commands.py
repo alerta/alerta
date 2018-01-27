4bec7a217301575a1ac245d934b957c9230ded85
Nick Satterly <nick.satterly@theguardian.com>
2018-01-27 18:57:45 +0000

    Add create API key command to alertad

--- alerta/commands.py
+++ alerta/commands.py
@@ -1,16 +1,44 @@
 
+import sys
 import click
-from flask.cli import FlaskGroup
+
+from flask import current_app
+from flask.cli import FlaskGroup, with_appcontext
+
+from alerta.app import create_app, db
+from alerta.models.key import ApiKey
 
 
-def make_app(info):
-    from alerta.app import create_app
-    return create_app()
-
-
-@click.group(cls=FlaskGroup, create_app=make_app)
+@click.group(cls=FlaskGroup, create_app=create_app)
 def cli():
-    """This is a development script for the Alerta server."""
+    pass
+
+
+@cli.command()
+@with_appcontext
+def key():
+    db.get_db()
+    for admin in current_app.config['ADMIN_USERS']:
+        key = ApiKey(
+            user=admin,
+            scopes=['admin', 'write', 'read'],
+            text='Admin key created by alertad script',
+            expire_time=None
+        )
+        try:
+            key = key.create()
+        except Exception as e:
+            click.echo('ERROR: {}'.format(e))
+            sys.exit(1)
+        click.echo('{} {}'.format(key.key, key.user))
+
+# @cli.command()
+# @with_appcontext
+# # @click.option('--coverage/--no-coverage', default=False, help='aaa')
+# # def test(coverage=False):
+# def test():
+#     click.echo('test')
+
 
 if __name__ == '__main__':
     cli()
