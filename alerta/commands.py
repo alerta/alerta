b3df31285734e1004686be178228bb9e8a78fb8b
Nick Satterly <nick.satterly@theguardian.com>
2018-01-27 19:28:47 +0000

    Add create admin users command to alertad

--- alerta/commands.py
+++ alerta/commands.py
@@ -1,12 +1,14 @@
 
 import sys
-import click
 
+import click
 from flask import current_app
 from flask.cli import FlaskGroup, with_appcontext
 
 from alerta.app import create_app, db
+from alerta.auth.utils import generate_password_hash
 from alerta.models.key import ApiKey
+from alerta.models.user import User
 
 
 @click.group(cls=FlaskGroup, create_app=create_app)
@@ -14,10 +16,10 @@ def cli():
     pass
 
 
-@cli.command()
+@cli.command('key', short_help='Create admin API keys')
 @with_appcontext
 def key():
-    db.get_db()
+    db.get_db()  # init db on global app context
     for admin in current_app.config['ADMIN_USERS']:
         key = ApiKey(
             user=admin,
@@ -32,12 +34,27 @@ def key():
             sys.exit(1)
         click.echo('{} {}'.format(key.key, key.user))
 
-# @cli.command()
-# @with_appcontext
-# # @click.option('--coverage/--no-coverage', default=False, help='aaa')
-# # def test(coverage=False):
-# def test():
-#     click.echo('test')
+
+@cli.command('user', short_help='Create admin users')
+@click.password_option()
+@with_appcontext
+def user(password):
+    db.get_db()  # init db on global app context
+    for admin in current_app.config['ADMIN_USERS']:
+        user = User(
+            name=admin,
+            email=admin,
+            password=generate_password_hash(password),
+            roles=None,
+            text='Admin user created by alertad script',
+            email_verified=True
+        )
+        try:
+            user = user.create()
+        except Exception as e:
+            click.echo('ERROR: {}'.format(e))
+            sys.exit(1)
+        click.echo('{} {}'.format(user.id, user.name))
 
 
 if __name__ == '__main__':
