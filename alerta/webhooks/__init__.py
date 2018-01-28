a143bf93a8f66d8906e25eb303640b6e1ab4b060
Nick Satterly <nick.satterly@theguardian.com>
2018-01-28 21:23:17 +0000

    Log request info for webhooks (#469)

--- alerta/webhooks/__init__.py
+++ alerta/webhooks/__init__.py
@@ -1,7 +1,17 @@
 
-from flask import Blueprint
+from flask import Blueprint, request, current_app
 
 webhooks = Blueprint('webhooks', __name__)
 
 from . import cloudwatch, grafana, graylog, newrelic, pagerduty, pingdom, prometheus, riemann
 from . import serverdensity, slack, stackdriver, telegram
+
+
+@webhooks.before_request
+def before_request():
+    current_app.logger.info('Webhook Request:\n{} {}\n\n{}{}'.format(
+        request.method,
+        request.url,
+        request.headers,
+        request.get_data().decode('utf-8')
+    ))
