d49c4029159144e7a6aff58f9f59bc0b7b7042bd
Nick Satterly <nfsatterly@gmail.com>
2018-04-02 16:00:13 +0200

    Add customer blackout test (#498)

--- tests/test_customers.py
+++ tests/test_customers.py
@@ -16,16 +16,23 @@ class AuthTestCase(unittest.TestCase):
             'AUTH_REQUIRED': True,
             'CUSTOMER_VIEWS': True,
             'ADMIN_USERS': ['admin@alerta.io'],
-            'ALLOWED_EMAIL_DOMAINS': ['alerta.io', 'example.com']
+            'ALLOWED_EMAIL_DOMAINS': ['alerta.io', 'foo.com', 'bar.com']
         }
         self.app = create_app(test_config)
         self.client = self.app.test_client()
 
-        self.alert = {
-            'event': 'Foo',
-            'resource': 'Bar',
+        self.foo_alert = {
+            'event': 'foo1',
+            'resource': 'foo1',
             'environment': 'Production',
-            'service': ['Quux']
+            'service': ['Web']
+        }
+
+        self.bar_alert = {
+            'event': 'bar1',
+            'resource': 'bar1',
+            'environment': 'Production',
+            'service': ['Web']
         }
 
         with self.app.test_request_context('/'):
@@ -47,19 +54,26 @@ class AuthTestCase(unittest.TestCase):
 
     def test_blackouts(self):
 
-        # add customer mapping
+        # add customer mappings
         payload = {
-            'customer': 'Example Corp',
-            'match': 'example.com'
+            'customer': 'Foo Corp',
+            'match': 'foo.com'
         }
         response = self.client.post('/customer', data=json.dumps(payload), content_type='application/json', headers=self.headers)
         self.assertEqual(response.status_code, 201)
 
-        # create user
         payload = {
-            'name': 'Example User',
-            'email': 'user@example.com',
-            'password': '3x8mpl3',
+            'customer': 'Bar Corp',
+            'match': 'bar.com'
+        }
+        response = self.client.post('/customer', data=json.dumps(payload), content_type='application/json', headers=self.headers)
+        self.assertEqual(response.status_code, 201)
+
+        # create users
+        payload = {
+            'name': 'Foo User',
+            'email': 'user@foo.com',
+            'password': 'f00f00',
             'text': ''
         }
 
@@ -68,22 +82,62 @@ class AuthTestCase(unittest.TestCase):
         data = json.loads(response.data.decode('utf-8'))
         self.assertIsNotNone(data, 'Failed to create user')
 
-        # use JWT token for user auth
-        token = data['token']
-        user_headers = {
-            'Authorization': 'Bearer %s' % token,
+        foo_user_headers = {
+            'Authorization': 'Bearer %s' % data['token'],
             'Content-type': 'application/json'
         }
 
-        # create user blackout
-        response = self.client.post('/blackout', data=json.dumps({"environment": "Production"}), headers=user_headers)
+        payload = {
+            'name': 'Bar User',
+            'email': 'user@bar.com',
+            'password': 'b8rb8r',
+            'text': ''
+        }
+
+        response = self.client.post('/auth/signup', data=json.dumps(payload), content_type='application/json', headers=self.headers)
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.data.decode('utf-8'))
+        self.assertIsNotNone(data, 'Failed to create user')
+
+        bar_user_headers = {
+            'Authorization': 'Bearer %s' % data['token'],
+            'Content-type': 'application/json'
+        }
+
+        # create customer blackout by foo user
+        response = self.client.post('/blackout', data=json.dumps({"environment": "Production"}), headers=foo_user_headers)
         self.assertEqual(response.status_code, 201)
         data = json.loads(response.data.decode('utf-8'))
 
         blackout_id = data['id']
 
-        # delete user blackout by admin
+        # new alert by foo user should be suppressed
+        response = self.client.post('/alert', data=json.dumps(self.foo_alert), headers=foo_user_headers)
+        self.assertEqual(response.status_code, 202)
+
+        # new alert by bar user should not be suppressed
+        response = self.client.post('/alert', data=json.dumps(self.bar_alert), headers=bar_user_headers)
+        self.assertEqual(response.status_code, 201)
+
+        # delete blackout by id
         response = self.client.delete('/blackout/' + blackout_id, headers=self.headers)
         self.assertEqual(response.status_code, 200)
 
+        # create global blackout by admin user
+        response = self.client.post('/blackout', data=json.dumps({"environment": "Production"}), headers=self.headers)
+        self.assertEqual(response.status_code, 201)
+        data = json.loads(response.data.decode('utf-8'))
 
+        blackout_id = data['id']
+
+        # new alert by foo user should be suppressed
+        response = self.client.post('/alert', data=json.dumps(self.foo_alert), headers=foo_user_headers)
+        self.assertEqual(response.status_code, 202)
+
+        # new alert by bar user should be suppressed
+        response = self.client.post('/alert', data=json.dumps(self.bar_alert), headers=bar_user_headers)
+        self.assertEqual(response.status_code, 202)
+
+        # delete blackout by id
+        response = self.client.delete('/blackout/' + blackout_id, headers=self.headers)
+        self.assertEqual(response.status_code, 200)
