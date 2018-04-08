e22af91170284ec41ff8181c759a55176b8f04f4
Nick Satterly <nfsatterly@gmail.com>
2018-04-08 21:14:33 +0200

    Update housekeeping for mongo to support auto-unshelving (#506)

--- alerta/database/backends/mongodb/base.py
+++ alerta/database/backends/mongodb/base.py
@@ -1052,12 +1052,24 @@ class Backend(Database):
         info_hours_ago = datetime.utcnow() - timedelta(hours=info_threshold)
         g.db.alerts.remove({"severity": "informational", "lastReceiveTime": {'$lt': info_hours_ago}})
 
-        # return list of alerts to be newly expired
+        # get list of alerts to be newly expired
         pipeline = [
             {'$project': {
                 "event": 1, "status": 1, "lastReceiveId": 1, "timeout": 1,
                 "expireTime": {'$add': ["$lastReceiveTime", {'$multiply': ["$timeout", 1000]}]}}
             },
-            {'$match': {"status": {'$ne': 'expired'}, "expireTime": {'$lt': datetime.utcnow()}, "timeout": {'$ne': 0}}}
+            {'$match': {"status": {'$nin': ['expired','shelved']}, "expireTime": {'$lt': datetime.utcnow()}, "timeout": {'$ne': 0}}}
         ]
-        return [(r['_id'], r['event'], r['lastReceiveId']) for r in g.db.alerts.aggregate(pipeline)]
+        expired = [(r['_id'], r['event'], 'expired', r['lastReceiveId']) for r in g.db.alerts.aggregate(pipeline)]
+
+        # get list of alerts to be unshelved
+        pipeline = [
+            {'$project': {
+                "event": 1, "status": 1, "lastReceiveId": 1, "timeout": 1,
+                "expireTime": {'$add': ["$lastReceiveTime", {'$multiply': ["$timeout", 1000]}]}}
+            },
+            {'$match': {"status": 'shelved', "expireTime": {'$lt': datetime.utcnow()}, "timeout": {'$ne': 0}}}
+        ]
+        unshelved = [(r['_id'], r['event'], 'open', r['lastReceiveId']) for r in g.db.alerts.aggregate(pipeline)]
+
+        return expired + unshelved
