37ecfebd426ef0e139c1b3470ce6d65be95cb134
Nick Satterly <nfsatterly@gmail.com>
2018-04-27 19:58:33 +0200

    Fix user permissions during sign-up and update (#526)

--- alerta/views/users.py
+++ alerta/views/users.py
@@ -1,8 +1,11 @@
 
-from flask import jsonify, request, current_app
+from uuid import uuid4
+
+from flask import jsonify, request, g, current_app
 from flask_cors import cross_origin
 
 from alerta.app import qb
+from alerta.auth.utils import is_authorized, create_token, get_customers, send_confirmation
 from alerta.auth.utils import permission
 from alerta.exceptions import ApiError
 from alerta.models.user import User
@@ -10,7 +13,47 @@ from alerta.utils.api import jsonp
 from . import api
 
 
-# NOTE: "user create" method is basic auth "sign up" method
+@api.route('/user', methods=['OPTIONS', 'POST'])
+@cross_origin()
+@permission('admin:users')
+@jsonp
+def create_user():
+    try:
+        user = User.parse(request.json)
+    except Exception as e:
+        raise ApiError(str(e), 400)
+
+    # check allowed domain
+    if is_authorized('ALLOWED_EMAIL_DOMAINS', groups=[user.domain]):
+        raise ApiError("unauthorized domain", 403)
+
+    if User.find_by_email(email=user.email):
+        raise ApiError("username already exists", 409)
+
+    try:
+        user = user.create()
+    except Exception as e:
+        ApiError(str(e), 500)
+
+    # if email verification is enforced, deny login and send email
+    if current_app.config['EMAIL_VERIFICATION'] and not user.email_verified:
+        hash = str(uuid4())
+        send_confirmation(user, hash)
+        user.set_email_hash(hash)
+        raise ApiError('email not verified', 401)
+
+    # check user is active
+    if user.status != 'active':
+        raise ApiError('user not active', 403)
+
+    # assign customers & update last login time
+    customers = get_customers(user.email, groups=[user.domain])
+    user.update_last_login()
+
+    # generate token
+    token = create_token(user.id, user.name, user.email, provider='basic', customers=customers,
+                         roles=user.roles, email=user.email, email_verified=user.email_verified)
+    return jsonify(token=token.tokenize)
 
 
 @api.route('/user/<user_id>', methods=['OPTIONS', 'PUT'])
@@ -35,15 +78,61 @@ def update_user(user_id):
         raise ApiError("failed to update user", 500)
 
 
-@api.route('/user/<user_id>/attributes', methods=['OPTIONS', 'PUT'])
+@api.route('/user/me', methods=['OPTIONS', 'PUT'])
 @cross_origin()
 @permission('write:users')
+@jsonp
+def update_me():
+    if not request.json:
+        raise ApiError("nothing to change", 400)
+
+    if 'roles' in request.json:
+        raise ApiError('not allowed to update roles', 400)
+    if 'email_verified' in request.json:
+        raise ApiError('not allowed to set email verified', 400)
+
+    user = User.find_by_email(g.user)
+
+    if not user:
+        raise ApiError("not found", 404)
+
+    if 'email' in request.json and User.find_by_email(request.json['email']):
+        raise ApiError("user with email already exists", 409)
+
+    if user.update(**request.json):
+        return jsonify(status="ok")
+    else:
+        raise ApiError("failed to update user", 500)
+
+
+@api.route('/user/<user_id>/attributes', methods=['OPTIONS', 'PUT'])
+@cross_origin()
+@permission('admin:users')
 @jsonp
 def update_user_attributes(user_id):
     if not request.json.get('attributes', None):
         raise ApiError("must supply 'attributes' as json data", 400)
 
     user = User.find_by_id(user_id)
+
+    if not user:
+        raise ApiError("not found", 404)
+
+    if user.update_attributes(request.json['attributes']):
+        return jsonify(status="ok")
+    else:
+        raise ApiError("failed to update attributes", 500)
+
+
+@api.route('/user/me/attributes', methods=['OPTIONS', 'PUT'])
+@cross_origin()
+@permission('write:users')
+@jsonp
+def update_me_attributes():
+    if not request.json.get('attributes', None):
+        raise ApiError("must supply 'attributes' as json data", 400)
+
+    user = User.find_by_email(g.user)
 
     if not user:
         raise ApiError("not found", 404)
@@ -81,7 +170,7 @@ def search_users():
 
 @api.route('/user/<user_id>', methods=['OPTIONS', 'DELETE'])
 @cross_origin()
-@permission('write:users')
+@permission('admin:users')
 @jsonp
 def delete_user(user_id):
     user = User.find_by_id(user_id)
