ad736f40159f0d2cce5edfb8f31d5a7d558822bd
Andreas Skoglund <anderen95@gmail.com>
2018-04-29 01:00:53 +0200

    Fix for issue where housekeeping has stopped working with PostgreSQL (#528)

--- alerta/models/alert.py
+++ alerta/models/alert.py
@@ -468,19 +468,26 @@ class Alert(object):
 
     @staticmethod
     def housekeeping(expired_threshold=2, info_threshold=12):
-        for (id, event, status, last_receive_id) in db.housekeeping(expired_threshold, info_threshold):
-            if status == 'open':
-                text = 'unshelved after timeout'
-            elif status == 'expired':
-                text = 'expired after timeout'
-            else:
-                text = 'alert timeout status change'
+        expired, unshelved = db.housekeeping(expired_threshold, info_threshold)
+
+        for (id, event, last_receive_id) in expired:
             history = History(
                 id=last_receive_id,
                 event=event,
-                status=status,
-                text=text,
+                status='expired',
+                text='expired after timeout',
                 change_type="status",
                 update_time=datetime.utcnow()
             )
-            db.set_status(id, status, timeout=current_app.config['ALERT_TIMEOUT'], history=history)
+            db.set_status(id, 'expired', timeout=current_app.config['ALERT_TIMEOUT'], history=history)
+
+        for (id, event, last_receive_id) in unshelved:
+            history = History(
+                id=last_receive_id,
+                event=event,
+                status='open',
+                text='unshelved after timeout',
+                change_type="status",
+                update_time=datetime.utcnow()
+            )
+            db.set_status(id, 'open', timeout=current_app.config['ALERT_TIMEOUT'], history=history)
