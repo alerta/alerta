7692dfabfbcbb26d42a0288b774203c524a333ee
Chris Lamb <chris.lamb@hulu.com>
2018-07-26 15:34:21 -0700

    Make SAML exception to content-type restriction more explicit

--- alerta/auth/__init__.py
+++ alerta/auth/__init__.py
@@ -15,5 +15,8 @@ from . import github, gitlab, google, keycloak, pingfederate, saml2, userinfo
 
 @auth.before_request
 def only_json():
-    if request.method in ['POST', 'PUT'] and not request.is_json and not request.path == '/auth/saml':
-        raise ApiError("POST and PUT requests must set 'Content-type' to 'application/json'", 415)
+    # SAML2 Assertion Consumer Service expects POST request with 'Content-Type': 'application/x-www-form-urlencoded' from IdP
+    if request.method == 'POST' and request.path == '/auth/saml' and request.headers['Content-Type'] == 'application/x-www-form-urlencoded':
+        return
+    if request.method in ['POST', 'PUT'] and not request.is_json:
+        raise ApiError("POST and PUT requests must set 'Content-Type' to 'application/json'", 415)
