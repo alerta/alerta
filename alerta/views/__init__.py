5817a33dda156f55be7f318c4c0fcfac1c8460d5
Nick Satterly <nfsatterly@gmail.com>
2018-09-17 00:29:21 +0200

    Add more settings to config endpoint (#656)

--- alerta/views/__init__.py
+++ alerta/views/__init__.py
@@ -34,20 +34,26 @@ def debug():
 
 @api.route('/config', methods=['GET'])
 def config():
-    return jsonify(
-        config={
-            'endpoint': current_app.config['BASE_URL'],
-            'provider': current_app.config['AUTH_PROVIDER'],
-            'signup_enabled': current_app.config['SIGNUP_ENABLED'],
-            'client_id': current_app.config['OAUTH2_CLIENT_ID'],
-            'github_url': current_app.config['GITHUB_URL'],
-            'gitlab_url': current_app.config['GITLAB_URL'],
-            'keycloak_url': current_app.config['KEYCLOAK_URL'],
-            'keycloak_realm': current_app.config['KEYCLOAK_REALM'],
-            'pingfederate_url': current_app.config['PINGFEDERATE_URL'],
-            'colors': {},  # not supported yet
-            'severity': current_app.config['SEVERITY_MAP'],
-            'tracking_id': current_app.config['GOOGLE_TRACKING_ID'],
-            'refresh_interval': current_app.config['AUTO_REFRESH_INTERVAL']
-        }
-    )
+    return jsonify({
+        'endpoint': absolute_url().rstrip('/'),  # FIXME - shouldn't need to rstrip()
+        'auth_required': current_app.config['AUTH_REQUIRED'],
+        'customer_views': current_app.config['CUSTOMER_VIEWS'],
+        'provider': current_app.config['AUTH_PROVIDER'],
+        'signup_enabled': current_app.config['SIGNUP_ENABLED'],
+        'client_id': current_app.config['OAUTH2_CLIENT_ID'],
+        'github_url': current_app.config['GITHUB_URL'],
+        'gitlab_url': current_app.config['GITLAB_URL'],
+        'keycloak_url': current_app.config['KEYCLOAK_URL'],
+        'keycloak_realm': current_app.config['KEYCLOAK_REALM'],
+        'pingfederate_url': current_app.config['PINGFEDERATE_URL'],
+        'colors': {},  # not supported yet
+        'severity': current_app.config['SEVERITY_MAP'],
+        'dates': {
+            'shortTime': 'shortTime',  # 1:39 PM
+            'mediumDate': 'medium',  # Apr 26, 2016 1:39:44 PM
+            'longDate': 'EEEE, MMMM d, yyyy h:mm:ss.sss a (Z)'  # Tuesday, April 26, 2016 1:39:43.987 PM (+0100)
+        },
+        'audio': {},  # not supported yet
+        'tracking_id': current_app.config['GOOGLE_TRACKING_ID'],
+        'refresh_interval': current_app.config['AUTO_REFRESH_INTERVAL']
+    })
