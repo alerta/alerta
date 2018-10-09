b199435dac443a0a52d6a63572e0b44e330f335f
Nick Satterly <nfsatterly@gmail.com>
2018-10-09 23:58:38 +0200

    Update flask logging to remove repeated log lines

--- alerta/utils/config.py
+++ alerta/utils/config.py
@@ -1,6 +1,9 @@
 
 import logging
 import os
+from logging.handlers import RotatingFileHandler
+
+from flask.logging import default_handler, wsgi_errors_stream
 
 
 class Config:
@@ -135,46 +138,45 @@ class Config:
 
     @staticmethod
     def setup_logging(app):
-        del app.logger.handlers[:]
-
-        # for key in logging.Logger.manager.loggerDict:
-        #     print(key)
-
-        loggers = [
-            app.logger,
-            logging.getLogger('alerta'),  # ??
-            # logging.getLogger('flask'),  # ??
-            logging.getLogger('flask_compress'),  # ??
-            # logging.getLogger('flask_cors'),  # ??
-            logging.getLogger('pymongo'),  # ??
-            logging.getLogger('raven'),  # ??
-            logging.getLogger('requests'),  # ??
-            logging.getLogger('sentry'),  # ??
-            logging.getLogger('urllib3'),  # ??
-            logging.getLogger('werkzeug'),  # ??
-        ]
-
-        if app.debug:
-            log_level = logging.DEBUG
-        else:
-            log_level = logging.INFO
+        app.logger.removeHandler(default_handler)
 
         if app.config['LOG_FILE']:
-            from logging.handlers import RotatingFileHandler
             handler = RotatingFileHandler(
                 filename=app.config['LOG_FILE'],
                 maxBytes=app.config['LOG_MAX_BYTES'],
                 backupCount=app.config['LOG_BACKUP_COUNT'],
                 encoding='utf-8'
             )
-            handler.setLevel(log_level)
-            handler.setFormatter(logging.Formatter(app.config['LOG_FORMAT']))
+            app.logger.addHandler(handler)
         else:
-            handler = logging.StreamHandler()
-            handler.setLevel(log_level)
-            handler.setFormatter(logging.Formatter(app.config['LOG_FORMAT']))
+            handler = logging.StreamHandler(wsgi_errors_stream)
+
+        if app.debug:
+            log_level = logging.DEBUG
+        else:
+            log_level = logging.INFO
+        handler.setLevel(log_level)
+        handler.setFormatter(logging.Formatter(app.config['LOG_FORMAT']))
+
+        # for key in sorted(logging.Logger.manager.loggerDict):
+        #     print('logging.getLogger(\'{}\'),'.format(key))
+
+        loggers = [
+            logging.getLogger('amqp'),
+            logging.getLogger('asyncio'),
+            logging.getLogger('celery'),
+            logging.getLogger('concurrent'),
+            logging.getLogger('flask'),
+            logging.getLogger('kombu'),
+            logging.getLogger('pymongo'),
+            logging.getLogger('raven'),
+            logging.getLogger('requests'),
+            logging.getLogger('sentry'),
+            logging.getLogger('urllib3'),
+            logging.getLogger('werkzeug'),
+        ]
 
         for logger in loggers:
-            logger.addHandler(handler)
-            logger.setLevel(log_level)
-            logger.propagate = True
+            if not logger.handlers:
+                logger.setLevel(log_level)
+                logger.addHandler(handler)
