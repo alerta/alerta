b88f31b6da48ac38658a633ff66e2a4f22b4f6af
Nick Satterly <nfsatterly@gmail.com>
2018-10-30 23:06:53 +0100

    Show custom webhooks in list of rules

--- alerta/utils/webhook.py
+++ alerta/utils/webhook.py
@@ -2,12 +2,18 @@
 from pkg_resources import iter_entry_points
 
 
+class WebhookRule:
+    def __init__(self, rule, endpoint, methods):
+        self.rule = rule
+        self.endpoint = endpoint
+        self.methods = methods
+
+
 class CustomWebhooks:
     def __init__(self):
         self.webhooks = dict()
 
     def register(self, app):
-
         for ep in iter_entry_points('alerta.webhooks'):
             try:
                 webhook = ep.load()
@@ -15,3 +21,11 @@ class CustomWebhooks:
                     self.webhooks[ep.name] = webhook()
             except Exception as e:
                 app.log.warn('Failed to load custom webhook {} - {}'.format(ep.name, e))
+
+    def iter_rules(self):
+        return iter([
+            WebhookRule(
+                rule='/webhooks/{}'.format(name),
+                endpoint='webhooks.{}'.format(name),
+                methods=['POST', 'GET']
+            ) for name, ep in self.webhooks.items()])
