e0f6e1229cead3592bfca91fc54d136cb8ba64ad
Nick Satterly <nfsatterly@gmail.com>
2019-01-14 20:43:21 +0100

    Lock version of pyparsing to 2.3.0 (#813)

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setuptools.setup(
         'raven[flask]>=6.2.1',
         'pymongo>=3.0',
         'psycopg2',
-        'pyparsing',
+        'pyparsing==2.3.0',
         'requests',
         'python-dateutil',
         'pytz',
