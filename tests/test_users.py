alerta/alerta

commit 09118991b0a2209051c9e33e522a55428692a4ab

Simplify edit user endpoint (#792)

alerta/models/user.py
@@ -146,29 +146,15 @@ class User:
         return db.update_last_login(self.id)
 
     def update(self, **kwargs) -> 'User':
-        update = dict()
-        if kwargs.get('name', None) is not None:
-            update['name'] = kwargs['name']
-        if kwargs.get('email', None) is not None:
+        if 'email' in kwargs:
             if '@' not in kwargs['email']:
                 raise ValueError('Value for "email" not valid: %s' % kwargs['email'])
-            update['email'] = kwargs['email']
-            update['email_verified'] = False
-        if kwargs.get('password', None) is not None:
-            update['password'] = utils.generate_password_hash(kwargs['password'])
-        if kwargs.get('status', None) is not None:
-            update['status'] = kwargs['status']
-        if kwargs.get('role', None) is not None:
-            update['roles'] = [kwargs['role']]
-        elif kwargs.get('roles', None) is not None:
-            update['roles'] = kwargs['roles']
-        if kwargs.get('attributes', None) is not None:
-            update['attributes'] = kwargs['attributes']
-        if kwargs.get('text', None) is not None:
-            update['text'] = kwargs['text']
-        if kwargs.get('email_verified', None) is not None:
-            update['email_verified'] = kwargs['email_verified']
-        return User.from_db(db.update_user(self.id, **update))
+            kwargs['email_verified'] = kwargs.get('email_verified', False)
+        if 'password' in kwargs:
+            kwargs['password'] = utils.generate_password_hash(kwargs['password'])
+        if 'role' in kwargs:
+            kwargs['roles'] = kwargs['role']  # backwards compat
+        return User.from_db(db.update_user(self.id, **kwargs))
 
     # update user attributes
     def update_attributes(self, attributes: Dict[str, Any]) -> bool:

commit 30269efd3b83ec00d909f1d684da5b3de692ea90

Fix user role regression introduced in #792

alerta/models/user.py
@@ -153,7 +153,7 @@ class User:
         if kwargs.get('password') is not None:
             kwargs['password'] = utils.generate_password_hash(kwargs['password'])
         if 'role' in kwargs:
-            kwargs['roles'] = kwargs['role']  # backwards compat
+            kwargs['roles'] = [kwargs['role']]  # backwards compat
         return User.from_db(db.update_user(self.id, **kwargs))
 
     # update user attributes

tests/test_users.py
@@ -1,0 +1,80 @@
+
+import json
+import unittest
+
+from alerta.app import create_app, db
+
+
+class UserTestCase(unittest.TestCase):
+
+    def setUp(self):
+
+        test_config = {
+            'TESTING': True,
+            'AUTH_REQUIRED': False
+        }
+        self.app = create_app(test_config)
+        self.client = self.app.test_client()
+
+    def tearDown(self):
+        db.destroy()
+
+    def test_user(self):
+
+        payload = {
+            'name': 'John Doe',
+            'email': 'john@doe.com',
+            'password': 'p8ssw0rd',
+            'roles': ['operator'],
+            'text': 'devops user'
+        }
+        headers = {
+            'Content-type': 'application/json'
+        }
+
+        # create user
+        response = self.client.post('/user', data=json.dumps(payload), headers=headers)
+        self.assertEqual(response.status_code, 201)
+        data = json.loads(response.data.decode('utf-8'))
+        self.assertEqual(data['user']['name'], 'John Doe')
+        self.assertEqual(data['user']['email'], 'john@doe.com')
+        self.assertEqual(data['user']['roles'], ['operator'])
+        self.assertEqual(data['user']['email_verified'], False)
+
+        user_id = data['id']
+
+        payload = {
+            'role': 'devops'
+        }
+
+        # modify user (assign different role)
+        response = self.client.put('/user/' + user_id, data=json.dumps(payload), headers=headers)
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.data.decode('utf-8'))
+
+        # get user
+        response = self.client.get('/user/' + user_id, headers=headers)
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.data.decode('utf-8'))
+        self.assertEqual(data['user']['name'], 'John Doe')
+        self.assertEqual(data['user']['email'], 'john@doe.com')
+        self.assertEqual(data['user']['roles'], ['devops'])
+        self.assertEqual(data['user']['email_verified'], False)
+
+        payload = {
+            'roles': ['devops', 'operator', 'user']
+        }
+
+        # modify user (assign multiple roles)
+        response = self.client.put('/user/' + user_id, data=json.dumps(payload), headers=headers)
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.data.decode('utf-8'))
+
+        # get user
+        response = self.client.get('/user/' + user_id, headers=headers)
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.data.decode('utf-8'))
+        self.assertEqual(data['user']['name'], 'John Doe')
+        self.assertEqual(data['user']['email'], 'john@doe.com')
+        self.assertEqual(data['user']['roles'], ['devops', 'operator', 'user'])
+        self.assertEqual(data['user']['email_verified'], False)