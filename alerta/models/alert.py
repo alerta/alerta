de349c026391b705b333c3a283bbd2164098a8c4
Wim Fabri <wim.fabri@xenit.eu>
2019-03-01 09:32:32 +0100

    fix for bulk-acknowledged alert re-opening (#859)

--- alerta/models/alert.py
+++ alerta/models/alert.py
@@ -540,6 +540,7 @@ class Alert:
             event=self.event,
             severity=self.severity,
             status=status,
+            value=self.value,
             text=text,
             change_type='status',
             update_time=datetime.utcnow()
