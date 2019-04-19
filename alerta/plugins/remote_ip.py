c6b8abf62a01398c0296fe029263df22641389ff
Nick Satterly <nfsatterly@gmail.com>
2019-04-19 20:33:37 +0200

    Convert add remote IP method into a plugin

---
+++ alerta/plugins/remote_ip.py
@@ -0,0 +1,30 @@
+import logging
+
+from flask import request
+
+from alerta.plugins import PluginBase
+
+LOG = logging.getLogger('alerta.plugins')
+
+
+class RemoteIpAddr(PluginBase):
+    """
+    Add originating IP address of HTTP client as an alert attribute. This information
+    can be used for debugging, access control, or generating geolocation data.
+    """
+
+    def pre_receive(self, alert):
+        if request.headers.getlist('X-Forwarded-For'):
+            alert.attributes.update(ip=request.headers.getlist('X-Forwarded-For')[0])
+        else:
+            alert.attributes.update(ip=request.remote_addr)
+        return alert
+
+    def post_receive(self, alert):
+        return
+
+    def status_change(self, alert, status, text):
+        return
+
+    def take_action(self, alert, action, text, **kwargs):
+        raise NotImplementedError
--- alerta/settings.py
+++ alerta/settings.py
@@ -177,7 +177,7 @@ GOOGLE_TRACKING_ID = None
 AUTO_REFRESH_INTERVAL = 5000  # ms
 
 # Plugins
-PLUGINS = ['reject', 'heartbeat', 'blackout']
+PLUGINS = ['remote_ip', 'reject', 'heartbeat', 'blackout']
 PLUGINS_RAISE_ON_ERROR = True  # raise RuntimeError exception on first failure
 
 # reject plugin settings
