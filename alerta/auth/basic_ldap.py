8499aebbc63ec0671d83b93ee4acb14c5ba15eda
slyngshede <simon@lyngshede.dk>
2019-07-28 20:05:20 +0200

    Allow LDAP servers to use a self signed certificate (#1035)
    * Allow LDAP servers to use a self signed certificate
    
    * Rename LDAP_ALLOW_SELF_SIGNED_CERT option

--- alerta/auth/basic_ldap.py
+++ alerta/auth/basic_ldap.py
@@ -16,6 +16,10 @@ from . import auth
 @auth.route('/auth/login', methods=['OPTIONS', 'POST'])
 @cross_origin(supports_credentials=True)
 def login():
+    # Allow LDAP server to use a self signed certificate
+    if current_app.config['LDAP_ALLOW_SELF_SIGNED_CERT']:
+        ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_ALLOW)
+
     # Retrieve required fields from client request
     try:
         login = request.json.get('username', None) or request.json['email']
