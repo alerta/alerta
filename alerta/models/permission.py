ca578133b228c6a0f66f54b48915d27ead322a4b
Nick Satterly <nfsatterly@gmail.com>
2019-09-22 13:39:19 +0200

    Check for invalid scopes (#1080)

--- alerta/models/permission.py
+++ alerta/models/permission.py
@@ -13,6 +13,10 @@ class Permission:
 
     def __init__(self, match: str, scopes: List[Scope], **kwargs) -> None:
 
+        for s in scopes:
+            if s not in list(Scope):
+                raise ValueError('invalid scope: {}'.format(s))
+
         self.id = kwargs.get('id', str(uuid4()))
         self.match = match
         self.scopes = scopes or list()
