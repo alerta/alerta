b268a3b32989cd7a4cd9b21fa557b39fa58d2b87
Nick Satterly <nfsatterly@gmail.com>
2020-02-13 23:29:01 +0100

    Add Python 3.8 to supported versions (#1140)
    * Add Python 3.8 to supported versions
    
    * Do not pin mypy version as not compatible with Python 3.8
    
    *  Fix mypy and other errors
    
    * Fix HTTPException typing issue

--- setup.py
+++ setup.py
@@ -94,6 +94,7 @@ setuptools.setup(
         'Intended Audience :: System Administrators',
         'Intended Audience :: Telecommunications Industry',
         'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.5',
