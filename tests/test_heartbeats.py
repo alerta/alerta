d8f09e928e75cf963d84f67325f421f961e3d965
Nick Satterly <nfsatterly@gmail.com>
2020-04-16 19:56:51 +0200

    Heartbeat event name config

--- tests/test_heartbeats.py
+++ tests/test_heartbeats.py
@@ -12,7 +12,9 @@ class HeartbeatsTestCase(unittest.TestCase):
         test_config = {
             'TESTING': True,
             'AUTH_REQUIRED': False,
-            'HEARTBEAT_TIMEOUT': 240
+            'HEARTBEAT_TIMEOUT': 240,
+            'HEARTBEAT_EVENTS': ['Heartbeat', 'Watchdog'],
+            'PLUGINS': ['heartbeat']
         }
         self.app = create_app(test_config)
         self.client = self.app.test_client()
@@ -106,7 +108,7 @@ class HeartbeatsTestCase(unittest.TestCase):
     def test_heartbeat_from_alert(self):
 
         heartbeat_alert = {
-            'event': 'Heartbeat',
+            'event': 'Watchdog',
             'resource': 'net01',
             'environment': 'Production',
             'service': ['Svc1'],
