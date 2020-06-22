f02196d840fee4c39b0131438708ef2e980b596a
Nick Satterly <nfsatterly@gmail.com>
2020-06-22 20:06:33 +0200

    End support for Python 3.5 (#1248)

--- setup.py
+++ setup.py
@@ -100,8 +100,7 @@ setuptools.setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.5',
         'Topic :: System :: Monitoring',
     ],
-    python_requires='>=3.5'
+    python_requires='>=3.6'
 )
