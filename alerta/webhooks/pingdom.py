ed78987858e4903017d5efc56d12d431c13c4796
Robert Malm Wiklund <robert.malmwiklund@schibsted.com>
2020-10-20 18:08:26 +0200

    ':' nog allowed in Pingdom tags, use '_' for split instead.

--- alerta/webhooks/pingdom.py
+++ alerta/webhooks/pingdom.py
@@ -29,7 +29,7 @@ class PingdomWebhook(WebhookBase):
             severity = alarm_model.DEFAULT_NORMAL_SEVERITY
 
         if len(payload['tags']) > 0:
-            tags_dict = { d[0].strip():d[1].strip() for d in [ t.split(':') for t in payload['tags'] if ':' in t ] }
+            tags_dict = { d[0].strip():d[1].strip() for d in [ t.split('_') for t in payload['tags'] if '_' in t ] }
 
             if 'environment' in tags_dict:
                 environment = tags_dict['environment']
