2022944277882f81aa396fd4ac5f89a7fe5f8aeb
Nick Satterly <nfsatterly@gmail.com>
2021-05-08 19:04:01 +0200

    feat: Add support for user-defined API key (#1518)
    * Support creation of user-defined API keys
    
    * Add test for user-defined API key

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -48,7 +48,8 @@ class AuthTestCase(unittest.TestCase):
             self.api_key = ApiKey(
                 user='admin@alerta.io',
                 scopes=[Scope.admin, Scope.read, Scope.write],
-                text='demo-key'
+                text='Demo API key - not for production use',
+                key='demo-key'
             )
             self.api_key.create()
 
@@ -65,6 +66,10 @@ class AuthTestCase(unittest.TestCase):
 
         response = self.client.get('/alerts')
         self.assertEqual(response.status_code, 401)
+
+    def test_user_defined_key(self):
+
+        self.assertEqual(self.api_key.key, 'demo-key')
 
     def test_admin_key(self):
 
