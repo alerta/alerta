78a6b18a15931bbf610f33ec7fbb6ca67625a578
Sai Varun Reddy Daram <saivarunvishal@gmail.com>
2021-11-24 19:41:53 +0530

    Miscs

--- alerta/stats/stats.py
+++ alerta/stats/stats.py
@@ -13,7 +13,7 @@ class StatsD:
 
     @staticmethod
     def timer(stat_name, start_time, tags: dict = {}):
-        stats_client.timing(stat_name, start_time, {**tags, })
+        stats_client.timing(stat_name, start_time, tags={**tags, })
 
     @staticmethod
     def gauge():
@@ -25,4 +25,4 @@ class StatsD:
 
     @staticmethod
     def increment(stat_name, value, tags: dict = {}):
-        stats_client.incr(stat_name, value, {**tags, })
+        stats_client.incr(stat_name, value, tags={**tags, })
