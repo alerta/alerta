bb39c4055d662d32bad1c5deafc71bb0801da860
Nick Satterly <nfsatterly@gmail.com>
2021-12-11 00:19:45 +0100

    Audit log should handle empty body on http delete

--- tests/test_logging.py
+++ tests/test_logging.py
@@ -1,9 +1,9 @@
 import json
 import unittest
 
-from alerta.app import create_app, custom_webhooks
-from alerta.models.alert import Alert
-from alerta.webhooks import WebhookBase
+import requests_mock
+
+from alerta.app import create_app, db
 
 
 class LoggingTestCase(unittest.TestCase):
@@ -16,34 +16,52 @@ class LoggingTestCase(unittest.TestCase):
             'LOG_HANDLERS': ['console'],
             'LOG_FORMAT': 'verbose',
             'AUDIT_TRAIL': ['admin', 'write', 'auth'],
-            'AUDIT_LOG': True
+            'AUDIT_LOG': True,
+            'AUDIT_LOG_JSON': True,
+            'AUDIT_URL': 'https://logs.alerta.dev'
         }
         self.app = create_app(test_config)
         self.client = self.app.test_client()
 
-    def test_custom_webhook(self):
+    def tearDown(self):
+        db.destroy()
 
-        # setup custom webhook
-        custom_webhooks.webhooks['json'] = DummyJsonWebhook()
+    def test_audit_log(self):
 
-        payload = """
-            {"baz": "quux %X %%"}
-        """
+        pass
 
-        # test json payload
-        response = self.client.post('/webhooks/json?foo=bar', json=json.loads(payload), content_type='application/json')
-        self.assertEqual(response.status_code, 201, response.data)
+    @requests_mock.mock()
+    def test_audit_url(self, m):
+
+        m.post('https://logs.alerta.dev')
+
+        # create blackout
+        response = self.client.post('/blackout', json={'environment': 'Production'})
+        self.assertEqual(response.status_code, 201)
         data = json.loads(response.data.decode('utf-8'))
-        self.assertEqual(data['alert']['resource'], 'bar')
-        self.assertEqual(data['alert']['event'], 'quux %X %%')
 
+        blackout_id = data['id']
 
-class DummyJsonWebhook(WebhookBase):
+        # Note: The content_type header should not need to be set for deletes, but ...
+        response = self.client.delete('/blackout/' + blackout_id, content_type='application/json')
+        self.assertEqual(response.status_code, 200)
 
-    def incoming(self, query_string, payload):
-        return Alert(
-            resource=query_string['foo'],
-            event=payload['baz'],
-            environment='Production',
-            service=['Foo']
-        )
+        create_blackout_request = json.loads(m.request_history[0].text)
+
+        self.assertEqual(create_blackout_request['event'], 'blackout-created')
+        self.assertEqual(create_blackout_request['category'], 'write')
+        self.assertEqual(create_blackout_request['resource']['type'], 'blackout')
+        self.assertEqual(create_blackout_request['request']['endpoint'], 'api.create_blackout')
+        self.assertEqual(create_blackout_request['request']['method'], 'POST')
+        self.assertEqual(create_blackout_request['request']['url'], 'http://localhost/blackout')
+        self.assertEqual(create_blackout_request['request']['data'], {'environment': 'Production'})
+
+        delete_blackout_request = json.loads(m.request_history[1].text)
+
+        self.assertEqual(delete_blackout_request['event'], 'blackout-deleted')
+        self.assertEqual(delete_blackout_request['category'], 'write')
+        self.assertEqual(delete_blackout_request['resource']['type'], 'blackout')
+        self.assertEqual(delete_blackout_request['request']['endpoint'], 'api.delete_blackout')
+        self.assertEqual(delete_blackout_request['request']['method'], 'DELETE')
+        self.assertTrue(delete_blackout_request['request']['url'].startswith('http://localhost/blackout/'))
+        self.assertEqual(delete_blackout_request['request']['data'], None)
