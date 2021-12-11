64b652dd6e73cdeedd957285d66d8ffd7b3d07d4
Nick Satterly <nfsatterly@gmail.com>
2021-12-11 17:38:36 +0100

    fix: Audit log should handle empty body on http delete (#1655)

--- alerta/utils/audit.py
+++ alerta/utils/audit.py
@@ -81,7 +81,7 @@ class AuditTrail:
              resource_id: str, type: str, request: Any, **extra: Any) -> str:
 
         def get_redacted_data(r):
-            data = r.get_json()
+            data = r.get_json(silent=True)
             if data and app.config['AUDIT_LOG_REDACT']:
                 if 'password' in data:
                     data['password'] = '[REDACTED]'
