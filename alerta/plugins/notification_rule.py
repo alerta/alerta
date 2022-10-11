d70fad6cd4f846b7aab5b1f33b28aa7333da5675
sbgap <sb@gapit.io>
2022-10-11 16:02:56 +0200

    fix: add logging of link_mobility_xml

--- alerta/plugins/notification_rule.py
+++ alerta/plugins/notification_rule.py
@@ -207,7 +207,7 @@ class NotificationRulesHandler(PluginBase):
             elif notification_type == 'link_mobility':
                 self.send_link_mobility_sms(message, channel, list(set([*notification_rule.receivers, *[f"{user.country_code}{user.phone_number}" for user in on_users]])), fernet)
             elif notification_type == 'link_mobility_xml':
-                self.send_link_mobility_xml(message, channel, list(set([*notification_rule.receivers, *[f"{user.country_code}{user.phone_number}" for user in on_users]])), fernet, xml=LINK_MOBILITY_XML)
+                LOG.debug(self.send_link_mobility_xml(message, channel, list(set([*notification_rule.receivers, *[f"{user.country_code}{user.phone_number}" for user in on_users]])), fernet, xml=LINK_MOBILITY_XML))
 
     def pre_receive(self, alert, **kwargs):
         return alert
